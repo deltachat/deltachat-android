e836e98833879dd6058d245fb140fae883025fc1
B. Petersen <r10s@b44t.com>
2016-07-03 13:52:53 +0200

    Remove BuildVars.java from .gitignore.

---
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -0,0 +1,17 @@
+/*
+ * This is the source code of Telegram for Android v. 3.x.x.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Nikolai Kudashov, 2013-2016.
+ */
+
+package org.telegram.messenger;
+
+public class BuildVars {
+    public static boolean DEBUG_VERSION = false;
+    public static int BUILD_VERSION = 787;
+    public static String BUILD_VERSION_STRING = "3.8";
+    public static int APP_ID = 12834;
+    public static String APP_HASH = "c84d9229db1d6be95c067b02b126352c";
+}
