d6bf6293f2cb565d55757c0346f46e790930c7ea
B. Petersen <r10s@b44t.com>
2016-09-16 18:52:00 +0200

    Add some basic routines.

--- TMessagesProj/jni/mr_wrapper.c
+++ TMessagesProj/jni/mr_wrapper.c
@@ -2,7 +2,7 @@
 #include <jni.h>
 
 
-JNIEXPORT int Java_org_telegram_messenger_MrMailbox_myJnixTest(JNIEnv *env, jclass class)
+JNIEXPORT int Java_org_telegram_messenger_MrMailbox_MrMailboxNew(JNIEnv *env, jclass class)
 {
-	return 24;
+	return 28;
 }
--- TMessagesProj/src/main/java/org/telegram/messenger/MrMailbox.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MrMailbox.java
@@ -4,5 +4,20 @@ package org.telegram.messenger;
  * Created by bpetersen on 14.09.16.
  */
 public class MrMailbox {
-    public native static int myJnixTest();
+
+    // work with mailboxes
+    public native static int     MrMailboxNew         (); // returns hMailbox (Names as mrmailbox_new don't work due to the additional underscore)
+    public native static void    MrMailboxDelete      (int hMailbox);
+    public native static int     MrMailboxOpen        (int hMailbox, String dbfile);
+    public native static void    MrMailboxClose       (int hMailbox);
+    public native static int     MrMailboxGetChats    (int hMailbox); // returns hChatlist
+
+    // working with chatlists
+    public native static int     MrChatlistGetCnt     (int hChatlist);
+    public native static int     MrChatlistGetChat    (int hChatlist, int index); // returns hChat
+
+    // working with chats
+    public native static int     MrChatGetId          (int hChat);
+    public native static int     MrChatGetType        (int hChat);
+    public native static String  MrChatCetName        (int hChat);
 }
