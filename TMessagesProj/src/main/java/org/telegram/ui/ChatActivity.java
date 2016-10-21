ce1a4e1bf52df1005bb924047fc1bc7d116b3b06
B. Petersen <r10s@b44t.com>
2016-10-21 17:23:24 +0200

    Enable mute options.

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -3830,7 +3830,49 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                 NotificationsController.updateServerNotificationsSettings(dialog_id);
                 NotificationsController.getInstance().removeNotificationsForDialog(dialog_id);
             } else {
-                // showDialog(AlertsCreator.createMuteAlert(getParentActivity(), dialog_id)); -- EDIT BY MR
+                // EDIT BY MR
+                AlertDialog.Builder builder = new AlertDialog.Builder(getParentActivity()); // was: BottomSheet.Builder
+                //builder.setTitle(LocaleController.getString("Notifications", R.string.Notifications)); -- a title seems more confusing than helping -- the user has clicked "mute" before and there are several options all starting with "mute...", I think, this is very clear
+                CharSequence[] items = new CharSequence[]{
+                        LocaleController.formatString("MuteFor", R.string.MuteFor, LocaleController.formatPluralString("Hours", 1)),
+                        LocaleController.formatString("MuteFor", R.string.MuteFor, LocaleController.formatPluralString("Hours", 8)),
+                        LocaleController.formatString("MuteFor", R.string.MuteFor, LocaleController.formatPluralString("Days", 2)),
+                        LocaleController.getString("MuteDisable", R.string.MuteDisable)
+                };
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                int untilTime = ConnectionsManager.getInstance().getCurrentTime();
+                                     if (i == 0) { untilTime += 60 * 60; }
+                                else if (i == 1) { untilTime += 60 * 60 * 8;  }
+                                else if (i == 2) { untilTime += 60 * 60 * 48; }
+                                else if (i == 3) { untilTime = Integer.MAX_VALUE; }
+
+                                SharedPreferences preferences = ApplicationLoader.applicationContext.getSharedPreferences("Notifications", Activity.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = preferences.edit();
+                                long flags;
+                                if (i == 3) {
+                                    editor.putInt("notify2_" + dialog_id, 2);
+                                    flags = 1;
+                                } else {
+                                    editor.putInt("notify2_" + dialog_id, 3);
+                                    editor.putInt("notifyuntil_" + dialog_id, untilTime);
+                                    flags = ((long) untilTime << 32) | 1;
+                                }
+                                NotificationsController.getInstance().removeNotificationsForDialog(dialog_id);
+                                MessagesStorage.getInstance().setDialogFlags(dialog_id, flags);
+                                editor.commit();
+                                TLRPC.TL_dialog dialog = MessagesController.getInstance().dialogs_dict.get(dialog_id);
+                                if (dialog != null) {
+                                    dialog.notify_settings = new TLRPC.TL_peerNotifySettings();
+                                    dialog.notify_settings.mute_until = untilTime;
+                                }
+                                NotificationsController.updateServerNotificationsSettings(dialog_id);
+                            }
+                        }
+                );
+                showDialog(builder.create());
+                // EDIT BY MR
             }
         } else {
             SharedPreferences preferences = ApplicationLoader.applicationContext.getSharedPreferences("Notifications", Activity.MODE_PRIVATE);
