8824b30c4a63f94ba2fdaeb5e40acc31cff546d8
B. Petersen <r10s@b44t.com>
2017-02-23 16:38:37 +0100

    Remove unused import.

--- MessengerProj/src/main/java/com/b44t/ui/DialogsActivity.java
+++ MessengerProj/src/main/java/com/b44t/ui/DialogsActivity.java
@@ -32,7 +32,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Outline;
