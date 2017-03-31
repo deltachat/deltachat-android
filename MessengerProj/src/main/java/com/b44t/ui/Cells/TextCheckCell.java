7e7d4176f2e1924c0d73818230f77752d90c967a
B. Petersen <r10s@b44t.com>
2017-03-31 11:36:54 +0200

    Adapt layout.

--- MessengerProj/src/main/java/com/b44t/ui/Cells/TextCheckCell.java
+++ MessengerProj/src/main/java/com/b44t/ui/Cells/TextCheckCell.java
@@ -81,7 +81,7 @@ public class TextCheckCell extends FrameLayout {
         checkBox.setFocusable(false);
         checkBox.setFocusableInTouchMode(false);
         checkBox.setClickable(false);
-        addView(checkBox, LayoutHelper.createFrame(LayoutHelper.WRAP_CONTENT, LayoutHelper.WRAP_CONTENT, (LocaleController.isRTL ? Gravity.LEFT : Gravity.RIGHT) | Gravity.CENTER_VERTICAL, 14, 0, 14, 0));
+        addView(checkBox, LayoutHelper.createFrame(LayoutHelper.WRAP_CONTENT, LayoutHelper.WRAP_CONTENT, (LocaleController.isRTL ? Gravity.LEFT : Gravity.RIGHT) | Gravity.CENTER_VERTICAL, 11, 0, 11, 0));
     }
 
     @Override
