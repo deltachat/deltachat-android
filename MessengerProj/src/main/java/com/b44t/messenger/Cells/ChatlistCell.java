48684bded44f188de2fb8be45a6a10eb78ecdce0
B. Petersen <r10s@b44t.com>
2017-10-28 17:06:54 +0200

    Check vector drawable support on KitKat, see https://stackoverflow.com/questions/37892573/android-vector-drawables-in-support-library-v24-0-0 - seems to work :)

--- MessengerProj/src/main/java/com/b44t/messenger/Cells/ChatlistCell.java
+++ MessengerProj/src/main/java/com/b44t/messenger/Cells/ChatlistCell.java
@@ -152,7 +152,7 @@ public class ChatlistCell extends BaseCell {
             countDrawable = getResources().getDrawable(R.drawable.dialogs_badge);
             groupDrawable = getResources().getDrawable(R.drawable.list_group);
             muteDrawable = getResources().getDrawable(R.drawable.mute_grey);
-            closeDrawable = getResources().getDrawable(R.drawable.ic_dismiss_deaddrop); // TODO: KitKat emulator crashes on first startup - because of the vector graphic?
+            closeDrawable = getResources().getDrawable(R.drawable.ic_dismiss_deaddrop);
         }
 
         avatarImage = new ImageReceiver(this);
