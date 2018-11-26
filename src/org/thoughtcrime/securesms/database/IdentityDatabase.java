ebcfd61e9ab378df84c8d9b6a5da7264f12ddd21
B. Petersen <r10s@b44t.com>
2018-11-26 23:08:26 +0100

    remove unverified|untrusted dialogs|banner

--- src/org/thoughtcrime/securesms/database/IdentityDatabase.java
+++ src/org/thoughtcrime/securesms/database/IdentityDatabase.java
@@ -16,21 +16,13 @@
  */
 package org.thoughtcrime.securesms.database;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 
 import net.sqlcipher.database.SQLiteDatabase;
 
 import org.thoughtcrime.securesms.database.helpers.SQLCipherOpenHelper;
-import org.thoughtcrime.securesms.util.Base64;
 import org.whispersystems.libsignal.IdentityKey;
-import org.whispersystems.libsignal.InvalidKeyException;
-import org.thoughtcrime.securesms.util.guava.Optional;
-
-import java.io.IOException;
 
 public class IdentityDatabase extends Database {
 
@@ -58,19 +50,6 @@ public class IdentityDatabase extends Database {
   public enum VerifiedStatus {
     DEFAULT, VERIFIED, UNVERIFIED;
 
-    public int toInt() {
-      if      (this == DEFAULT)    return 0;
-      else if (this == VERIFIED)   return 1;
-      else if (this == UNVERIFIED) return 2;
-      else throw new AssertionError();
-    }
-
-    public static VerifiedStatus forState(int state) {
-      if      (state == 0) return DEFAULT;
-      else if (state == 1) return VERIFIED;
-      else if (state == 2) return UNVERIFIED;
-      else throw new AssertionError("No such state: " + state);
-    }
   }
 
   IdentityDatabase(Context context, SQLCipherOpenHelper databaseHelper) {
@@ -82,83 +61,6 @@ public class IdentityDatabase extends Database {
     return database.query(TABLE_NAME, null, null, null, null, null, null);
   }
 
-  public @Nullable IdentityReader readerFor(@Nullable Cursor cursor) {
-    if (cursor == null) return null;
-    return new IdentityReader(cursor);
-  }
-
-  public Optional<IdentityRecord> getIdentity(Address address) {
-    SQLiteDatabase database = databaseHelper.getReadableDatabase();
-    Cursor         cursor   = null;
-
-    try {
-      cursor = database.query(TABLE_NAME, null, ADDRESS + " = ?",
-                              new String[] {address.serialize()}, null, null, null);
-
-      if (cursor != null && cursor.moveToFirst()) {
-        return Optional.of(getIdentityRecord(cursor));
-      }
-    } catch (InvalidKeyException | IOException e) {
-      throw new AssertionError(e);
-    } finally {
-      if (cursor != null) cursor.close();
-    }
-
-    return Optional.absent();
-  }
-
-  public void saveIdentity(Address address, IdentityKey identityKey, VerifiedStatus verifiedStatus,
-                           boolean firstUse, long timestamp, boolean nonBlockingApproval)
-  {
-    SQLiteDatabase database          = databaseHelper.getWritableDatabase();
-    String         identityKeyString = Base64.encodeBytes(identityKey.serialize());
-
-    ContentValues contentValues = new ContentValues();
-    contentValues.put(ADDRESS, address.serialize());
-    contentValues.put(IDENTITY_KEY, identityKeyString);
-    contentValues.put(TIMESTAMP, timestamp);
-    contentValues.put(VERIFIED, verifiedStatus.toInt());
-    contentValues.put(NONBLOCKING_APPROVAL, nonBlockingApproval ? 1 : 0);
-    contentValues.put(FIRST_USE, firstUse ? 1 : 0);
-
-    database.replace(TABLE_NAME, null, contentValues);
-  }
-
-  public void setApproval(Address address, boolean nonBlockingApproval) {
-    SQLiteDatabase database = databaseHelper.getWritableDatabase();
-
-    ContentValues contentValues = new ContentValues(2);
-    contentValues.put(NONBLOCKING_APPROVAL, nonBlockingApproval);
-
-    database.update(TABLE_NAME, contentValues, ADDRESS + " = ?", new String[] {address.serialize()});
-  }
-
-  public void setVerified(Address address, IdentityKey identityKey, VerifiedStatus verifiedStatus) {
-    SQLiteDatabase database = databaseHelper.getWritableDatabase();
-
-    ContentValues contentValues = new ContentValues(1);
-    contentValues.put(VERIFIED, verifiedStatus.toInt());
-
-    int updated = database.update(TABLE_NAME, contentValues, ADDRESS + " = ? AND " + IDENTITY_KEY + " = ?",
-                                  new String[] {address.serialize(), Base64.encodeBytes(identityKey.serialize())});
-
-    if (updated > 0) {
-      Optional<IdentityRecord> record = getIdentity(address);
-    }
-  }
-
-  private IdentityRecord getIdentityRecord(@NonNull Cursor cursor) throws IOException, InvalidKeyException {
-    String      address             = cursor.getString(cursor.getColumnIndexOrThrow(ADDRESS));
-    String      serializedIdentity  = cursor.getString(cursor.getColumnIndexOrThrow(IDENTITY_KEY));
-    long        timestamp           = cursor.getLong(cursor.getColumnIndexOrThrow(TIMESTAMP));
-    int         verifiedStatus      = cursor.getInt(cursor.getColumnIndexOrThrow(VERIFIED));
-    boolean     nonblockingApproval = cursor.getInt(cursor.getColumnIndexOrThrow(NONBLOCKING_APPROVAL)) == 1;
-    boolean     firstUse            = cursor.getInt(cursor.getColumnIndexOrThrow(FIRST_USE))            == 1;
-    IdentityKey identity            = new IdentityKey(Base64.decode(serializedIdentity), 0);
-
-    return new IdentityRecord(Address.fromSerialized(address), identity, VerifiedStatus.forState(verifiedStatus), firstUse, timestamp, nonblockingApproval);
-  }
-
   public static class IdentityRecord {
 
     private final Address        address;
@@ -166,7 +68,6 @@ public class IdentityDatabase extends Database {
     private final VerifiedStatus verifiedStatus;
     private final boolean        firstUse;
     private final long           timestamp;
-    private final boolean        nonblockingApproval;
 
     private IdentityRecord(Address address,
                            IdentityKey identitykey, VerifiedStatus verifiedStatus,
@@ -177,31 +78,14 @@ public class IdentityDatabase extends Database {
       this.verifiedStatus      = verifiedStatus;
       this.firstUse            = firstUse;
       this.timestamp           = timestamp;
-      this.nonblockingApproval = nonblockingApproval;
     }
 
     public Address getAddress() {
       return address;
     }
 
-    public IdentityKey getIdentityKey() {
-      return identitykey;
-    }
-
     public long getTimestamp() {
       return timestamp;
-    }
-
-    public VerifiedStatus getVerifiedStatus() {
-      return verifiedStatus;
-    }
-
-    public boolean isApprovedNonBlocking() {
-      return nonblockingApproval;
-    }
-
-    public boolean isFirstUse() {
-      return firstUse;
     }
 
     @Override
@@ -210,29 +94,4 @@ public class IdentityDatabase extends Database {
     }
 
   }
-
-  public class IdentityReader {
-    private final Cursor cursor;
-
-    IdentityReader(@NonNull Cursor cursor) {
-      this.cursor = cursor;
-    }
-
-    public @Nullable IdentityRecord getNext() {
-      if (cursor.moveToNext()) {
-        try {
-          return getIdentityRecord(cursor);
-        } catch (IOException | InvalidKeyException e) {
-          throw new AssertionError(e);
-        }
-      }
-
-      return null;
-    }
-
-    public void close() {
-      cursor.close();
-    }
-  }
-
 }
