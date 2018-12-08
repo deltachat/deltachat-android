5da75c7dd0e390b544058fbb99c1c25c60c63e4e
B. Petersen <r10s@b44t.com>
2018-12-08 18:22:08 +0100

    remove AttachmentDatabase loadings from PartAuthority

--- src/org/thoughtcrime/securesms/mms/PartAuthority.java
+++ src/org/thoughtcrime/securesms/mms/PartAuthority.java
@@ -24,8 +24,6 @@ public class PartAuthority {
   private static final Uri    PART_CONTENT_URI  = Uri.parse(PART_URI_STRING);
   private static final Uri    THUMB_CONTENT_URI = Uri.parse(THUMB_URI_STRING);
 
-  private static final int PART_ROW       = 1;
-  private static final int THUMB_ROW      = 2;
   private static final int PERSISTENT_ROW = 3;
   private static final int SINGLE_USE_ROW = 4;
 
@@ -33,8 +31,6 @@ public class PartAuthority {
 
   static {
     uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-    uriMatcher.addURI("org.thoughtcrime.securesms", "part/*/#", PART_ROW);
-    uriMatcher.addURI("org.thoughtcrime.securesms", "thumb/*/#", THUMB_ROW);
     uriMatcher.addURI(PersistentBlobProvider.AUTHORITY, PersistentBlobProvider.EXPECTED_PATH_OLD, PERSISTENT_ROW);
     uriMatcher.addURI(PersistentBlobProvider.AUTHORITY, PersistentBlobProvider.EXPECTED_PATH_NEW, PERSISTENT_ROW);
     uriMatcher.addURI(SingleUseBlobProvider.AUTHORITY, SingleUseBlobProvider.PATH, SINGLE_USE_ROW);
@@ -46,8 +42,6 @@ public class PartAuthority {
     int match = uriMatcher.match(uri);
     try {
       switch (match) {
-      case PART_ROW:       return DatabaseFactory.getAttachmentDatabase(context).getAttachmentStream(new PartUriParser(uri).getPartId(), 0);
-      case THUMB_ROW:      return DatabaseFactory.getAttachmentDatabase(context).getThumbnailStream(new PartUriParser(uri).getPartId());
       case PERSISTENT_ROW: return PersistentBlobProvider.getInstance(context).getStream(context, ContentUris.parseId(uri));
       case SINGLE_USE_ROW: return SingleUseBlobProvider.getInstance().getStream(ContentUris.parseId(uri));
       default:             return context.getContentResolver().openInputStream(uri);
@@ -61,12 +55,6 @@ public class PartAuthority {
     int match = uriMatcher.match(uri);
 
     switch (match) {
-    case THUMB_ROW:
-    case PART_ROW:
-      Attachment attachment = DatabaseFactory.getAttachmentDatabase(context).getAttachment(new PartUriParser(uri).getPartId());
-
-      if (attachment != null) return attachment.getFileName();
-      else                    return null;
     case PERSISTENT_ROW:
       return PersistentBlobProvider.getFileName(context, uri);
     case SINGLE_USE_ROW:
@@ -79,12 +67,6 @@ public class PartAuthority {
     int match = uriMatcher.match(uri);
 
     switch (match) {
-      case THUMB_ROW:
-      case PART_ROW:
-        Attachment attachment = DatabaseFactory.getAttachmentDatabase(context).getAttachment(new PartUriParser(uri).getPartId());
-
-        if (attachment != null) return attachment.getSize();
-        else                    return null;
       case PERSISTENT_ROW:
         return PersistentBlobProvider.getFileSize(context, uri);
       case SINGLE_USE_ROW:
@@ -97,12 +79,6 @@ public class PartAuthority {
     int match = uriMatcher.match(uri);
 
     switch (match) {
-      case THUMB_ROW:
-      case PART_ROW:
-        Attachment attachment = DatabaseFactory.getAttachmentDatabase(context).getAttachment(new PartUriParser(uri).getPartId());
-
-        if (attachment != null) return attachment.getContentType();
-        else                    return null;
       case PERSISTENT_ROW:
         return PersistentBlobProvider.getMimeType(context, uri);
       case SINGLE_USE_ROW:
@@ -129,8 +105,6 @@ public class PartAuthority {
   public static boolean isLocalUri(final @NonNull Uri uri) {
     int match = uriMatcher.match(uri);
     switch (match) {
-    case PART_ROW:
-    case THUMB_ROW:
     case PERSISTENT_ROW:
     case SINGLE_USE_ROW:
       return true;
