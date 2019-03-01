3844837569d54963a34c2922f685c50255c20377
B. Petersen <r10s@b44t.com>
2019-03-01 01:04:39 +0100

    basically compile android apk with rpgp using hardcoded paths

--- jni/Android.mk
+++ jni/Android.mk
@@ -851,6 +851,27 @@ LOCAL_SRC_FILES     := \
 
 include $(BUILD_STATIC_LIBRARY)
 
+
+################################################################################
+# rpgp
+################################################################################
+
+# rpgp must be cloned to the same directory where deltachat-android is
+# (not: inside deltachat-android)
+#
+# you can then cross-compile rpgp to android using
+# > cross build --release --target armv7-linux-androideabi -p pgp-ffi --features nightly
+#
+# TODO: x86 and other processor targets are still missing, automation, -droid script
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := librpgp
+LOCAL_SRC_FILES := ../../rpgp/target/armv7-linux-androideabi/release/libpgp_ffi.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+
 ################################################################################
 # main shared library as used from Java (includes the static ones)
 ################################################################################
@@ -866,13 +887,15 @@ $(JNI_DIR)/messenger-backend/libs/netpgp/include \
 $(JNI_DIR)/messenger-backend/libs/sqlite
 
 LOCAL_LDLIBS 	:= -ljnigraphics -llog -lz -latomic
-LOCAL_STATIC_LIBRARIES :=  etpan sasl2 sqlite crypto libiconv
+LOCAL_STATIC_LIBRARIES :=  etpan sasl2 sqlite crypto libiconv librpgp
 # if you get "undefined reference" errors, the reason for this may be the _order_! Eg. libiconv as the first library does not work!
 # "breakpad" was placed after "crypto", NativeLoader.cpp after dc_wrapper.c
 
 LOCAL_CFLAGS 	:= -w -Os -DNULL=0 -DSOCKLEN_T=socklen_t -DLOCALE_NOT_USED -D_LARGEFILE_SOURCE=1 -D_FILE_OFFSET_BITS=64
 LOCAL_CFLAGS 	+= -Drestrict='' -D__EMX__ -DOPUS_BUILD -DFIXED_POINT -DUSE_ALLOCA -DHAVE_LRINT -DHAVE_LRINTF -fno-math-errno -std=c99
 LOCAL_CFLAGS 	+= -DANDROID_NDK -DDISABLE_IMPORTGL -fno-strict-aliasing -fprefetch-loop-arrays -DAVOID_TABLES -DANDROID_TILE_BASED_DECODE -DANDROID_ARMV6_IDCT -ffast-math -D__STDC_CONSTANT_MACROS
+LOCAL_CFLAGS    += -DDC_USE_RPGP
+LOCAL_C_INCLUDES += $(JNI_DIR)/../../rpgp/pgp-ffi/
 
 LOCAL_SRC_FILES := \
 utils/org_thoughtcrime_securesms_util_FileUtils.cpp \
