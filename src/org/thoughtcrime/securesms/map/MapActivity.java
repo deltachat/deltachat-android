44219ca6f2cf1d43bd21d8b99ec64913317757dd
cyBerta <cyberta@riseup.net>
2019-03-08 11:34:18 +0100

    cleanup in MapActivity

--- src/org/thoughtcrime/securesms/map/MapActivity.java
+++ src/org/thoughtcrime/securesms/map/MapActivity.java
@@ -34,14 +34,7 @@ public class MapActivity extends BaseActivity implements Observer {
     public static final String TAG = MapActivity.class.getSimpleName();
     public static final String CHAT_ID = "chat_id";
 
-    public static final String LINE_LAYER = "line_layer";
-    public static final String LINE_SOURCE = "line_source";
-    public static final String MARKER_LAYER = "symbol_layer";
-    public static final String MARKER_ICON = "marker_icon_id";
-    public static final String MARKER_POSITION_SOURCE = "marker_position";
-
     private MapView mapView;
-    private MapboxMap mapboxMap;
     private DcLocation dcLocation;
     private MapDataManager mapDataManager;
 
@@ -59,7 +52,6 @@ public class MapActivity extends BaseActivity implements Observer {
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(mapboxMap -> mapboxMap.setStyle(Style.MAPBOX_STREETS, style -> {
-            this.mapboxMap = mapboxMap;
 
             mapboxMap.setCameraPosition(new CameraPosition.Builder()
                     .target(new LatLng(dcLocation.getLastLocation().getLatitude(), dcLocation.getLastLocation().getLongitude()))
@@ -78,7 +70,6 @@ public class MapActivity extends BaseActivity implements Observer {
 
         dcLocation = DcLocation.getInstance();
     }
-
 
     @Override
     protected void onStart() {
@@ -122,37 +113,6 @@ public class MapActivity extends BaseActivity implements Observer {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
-    }
-
-    private void initMapDrawings() {
-        if (mapboxMap == null || mapboxMap.getStyle() == null) {
-            return;
-        }
-        Style style = mapboxMap.getStyle();
-        initGeoJsonSources(style);
-        initLayers(style);
-    }
-
-    private void initGeoJsonSources(Style style) {
-        GeoJsonSource markerPositionSource = new GeoJsonSource(MARKER_POSITION_SOURCE);
-        GeoJsonSource linePositionSource = new GeoJsonSource(LINE_SOURCE);
-        style.addSource(markerPositionSource);
-        style.addSource(linePositionSource);
-    }
-
-    private void initLayers(Style style) {
-        style.addImage(MARKER_ICON,
-                BitmapFactory.decodeResource(
-                        MapActivity.this.getResources(), R.drawable.mapbox_marker_icon_default));
-        style.addLayer(new SymbolLayer(MARKER_LAYER, MARKER_POSITION_SOURCE)
-                .withProperties(PropertyFactory.iconImage(MARKER_ICON)));
-
-        style.addLayer(new LineLayer(LINE_LAYER, LINE_SOURCE)
-                .withProperties(PropertyFactory.lineCap(Property.LINE_CAP_ROUND),
-                        PropertyFactory.lineJoin(Property.LINE_JOIN_MITER),
-                        PropertyFactory.lineOpacity(.7f),
-                        PropertyFactory.lineWidth(7f),
-                        PropertyFactory.lineColor(Color.parseColor("#3bb2d0"))));
     }
 
     @Override
