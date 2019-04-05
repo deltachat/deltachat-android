631fa8a1d8c444e550295786cb0ba5da978dfc7a
B. Petersen <r10s@b44t.com>
2019-04-05 11:03:32 +0200

    tweak member-delete icon for dark-theme

--- src/org/thoughtcrime/securesms/util/DynamicTheme.java
+++ src/org/thoughtcrime/securesms/util/DynamicTheme.java
@@ -1,6 +1,7 @@
 package org.thoughtcrime.securesms.util;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 
 import org.thoughtcrime.securesms.R;
@@ -35,8 +36,8 @@ public class DynamicTheme {
     return R.style.TextSecure_LightTheme;
   }
 
-  public static boolean isDarkTheme(Activity activity) {
-    return Prefs.getTheme(activity).equals(DARK);
+  public static boolean isDarkTheme(Context context) {
+    return Prefs.getTheme(context).equals(DARK);
   }
 
   private static final class OverridePendingTransition {
