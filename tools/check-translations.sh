4027e84ff824744c5621611a5168028bc6671983
B. Petersen <r10s@b44t.com>
2019-06-29 12:01:24 +0200

    make check-translations.sh work on bsd

--- tools/check-translations.sh
+++ tools/check-translations.sh
@@ -2,6 +2,6 @@ echo potential errors, if any:
 
 # a space after the percent sign 
 # results in an IllegalFormatException in getString()
-grep --include='strings.xml' -r '\% '
-grep --include='strings.xml' -r '\$ '
-grep --include='strings.xml' -r ' \$'
+grep --include='strings.xml' -r '\% ' .
+grep --include='strings.xml' -r '\$ ' .
+grep --include='strings.xml' -r ' \$' .
