6a9a160f08917ddcd8b62c927473d5d51f9673ee
B. Petersen <r10s@b44t.com>
2019-10-02 20:57:13 +0200

    update core-update script

--- tools/update-core.sh
+++ tools/update-core.sh
@@ -1,38 +1,22 @@
 
-# this script updates the deltachat-core sub-repository from github.
+# this script updates the deltachat-core-rust sub-repository from github.
 # must be executed from the repo root.
 #
 # - make sure, the deltachat-android directory is clean
-# - make sure, deltachat-core is committed successfull before calling this script
-# - the script assumes, deltachat-core is placed in the same directory as deltachat-android
-#
-# to simplify core-development, files in the submodule folder are replaced by 
-# symbolic links to ../deltachat-core afterwards
-
-# remove links to the files
-rm -r jni/messenger-backend/*
-rm -r jni/messenger-backend/.??*
+# - make sure, deltachat-core-rust is committed successfully before calling this script
 
 # check out submodules as present in the repository
 git submodule update --init --recursive
 
 # update submodule
-cd jni/messenger-backend
+cd jni/deltachat-core-rust
 git checkout master
 git pull
 cd ../..
 
 # commit changes
-git add jni/messenger-backend/
-git commit -m "Update messenger-backend submodule."
+git add jni/deltachat-core-rust
+git commit -m "update deltachat-core-rust submodule"
 
-# remove files downloaded just  for committing
-rm -r jni/messenger-backend/*
-rm -r jni/messenger-backend/.??*
-
-# re-link all files (symbolic links may contain arbitrary text, so no relative paths, please)
-cd ..
-ln --symbolic `pwd`/deltachat-core/src     deltachat-android/jni/messenger-backend/src
-ln --symbolic `pwd`/deltachat-core/libs    deltachat-android/jni/messenger-backend/libs
-cd deltachat-android
-
+echo "changes are commited to local repo."
+echo "use 'git push' to use them or 'git reset HEAD~1; git submodule update --recursive' to abort on your own risk :)"
