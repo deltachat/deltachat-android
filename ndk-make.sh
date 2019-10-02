2d8db42ded5d6a41255385e8d74ac0b367741189
B. Petersen <r10s@b44t.com>
2019-10-02 21:50:16 +0200

    add x86_64 target

--- ndk-make.sh
+++ ndk-make.sh
@@ -2,11 +2,15 @@ echo -- cross compiling --
 cd jni/deltachat-core-rust
 
 # to setup the toolchains (from https://medium.com/visly/rust-on-android-19f34a2fb43 )
-# $ rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android
+# run the following in `jni/deltachat-core-rust`:
+# $ rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
+#
+# then:
 # $ mkdir ~/.NDK
 # $ $ANDROID_NDK/build/tools/make_standalone_toolchain.py --api 21 --arch arm64 --install-dir ~/.NDK/arm64
 # $ $ANDROID_NDK/build/tools/make_standalone_toolchain.py --api 14 --arch arm --install-dir ~/.NDK/arm
 # $ $ANDROID_NDK/build/tools/make_standalone_toolchain.py --api 14 --arch x86 --install-dir ~/.NDK/x86
+# $ $ANDROID_NDK/build/tools/make_standalone_toolchain.py --api 21 --arch x86_64 --install-dir ~/.NDK/x86_64
 # (--api should be aligned with APP_PLATFORM from Application.mk and with minSdkVersion)
 # this installs toolchains in `~/.NDK`
 # then, the following should work:
@@ -23,6 +27,10 @@ export RUSTFLAGS="-C linker=$HOME/.NDK/x86/bin/clang";
 export PATH=~/.NDK/x86/bin:$PATH;
 cargo build --release --target i686-linux-android -p deltachat_ffi
 
+export RUSTFLAGS="-C linker=$HOME/.NDK/x86_64/bin/clang";
+export PATH=~/.NDK/x86_64/bin:$PATH;
+cargo build --release --target x86_64-linux-android -p deltachat_ffi
+
 # an alternative might be:
 # $ cross build --release --target <target> -p deltachat_ffi
 
@@ -31,12 +39,15 @@ cd ..
 rm arm64-v8a/*
 rm armeabi-v7a/*
 rm x86/*
+rm x86_64/*
 mkdir -p arm64-v8a
 mkdir -p armeabi-v7a
 mkdir -p x86
+mkdir -p x86_64
 cp deltachat-core-rust/target/aarch64-linux-android/release/libdeltachat.a arm64-v8a
 cp deltachat-core-rust/target/armv7-linux-androideabi/release/libdeltachat.a armeabi-v7a
 cp deltachat-core-rust/target/i686-linux-android/release/libdeltachat.a x86
+cp deltachat-core-rust/target/x86_64-linux-android/release/libdeltachat.a x86_64
 
 echo -- ndk-build --
 cd ..
