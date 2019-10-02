baf93578405d3b17dbba334cded6806bc1c39796
B. Petersen <r10s@b44t.com>
2019-10-02 20:52:58 +0200

    switch rust-linker to clang

--- ndk-make.sh
+++ ndk-make.sh
@@ -8,11 +8,19 @@ cd jni/deltachat-core-rust
 # $ $ANDROID_NDK/build/tools/make_standalone_toolchain.py --api 14 --arch arm --install-dir ~/.NDK/arm
 # $ $ANDROID_NDK/build/tools/make_standalone_toolchain.py --api 14 --arch x86 --install-dir ~/.NDK/x86
 # (--api should be aligned with APP_PLATFORM from Application.mk and with minSdkVersion)
-# this installs toolchains in `~/.NDK`, make sure, the subdirs are in the $PATH variable.
+# this installs toolchains in `~/.NDK`
 # then, the following should work:
 
+export RUSTFLAGS="-C linker=$HOME/.NDK/arm64/bin/clang";
+export PATH=~/.NDK/arm64/bin:$PATH;
 cargo build --release --target aarch64-linux-android -p deltachat_ffi
+
+export RUSTFLAGS="-C linker=$HOME/.NDK/arm/bin/clang";
+export PATH=~/.NDK/arm/bin:$PATH;
 cargo build --release --target armv7-linux-androideabi -p deltachat_ffi
+
+export RUSTFLAGS="-C linker=$HOME/.NDK/x86/bin/clang";
+export PATH=~/.NDK/x86/bin:$PATH;
 cargo build --release --target i686-linux-android -p deltachat_ffi
 
 # an alternative might be:
