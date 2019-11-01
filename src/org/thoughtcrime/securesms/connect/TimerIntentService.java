9dea1ea3e1b0216641b734105708a8e49c30d2d3
cyberta <cyberta@riseup.net>
2019-11-01 18:38:42 +0100

    remove unused imports

--- src/org/thoughtcrime/securesms/connect/TimerIntentService.java
+++ src/org/thoughtcrime/securesms/connect/TimerIntentService.java
@@ -6,8 +6,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.os.Handler;
-import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.v4.app.JobIntentService;
 import android.util.Log;
