638a8e520537ca204cd46a7e116bf116d52b2427
B. Petersen <r10s@b44t.com>
2019-11-14 14:52:56 +0100

    adapt to ndk r20b

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,4 @@
-APP_PLATFORM := android-14
+APP_PLATFORM := android-21
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
-NDK_TOOLCHAIN_VERSION := 4.9
-APP_STL := gnustl_static
+APP_STL := c++_static
 
--- ndk-make.sh
+++ ndk-make.sh
@@ -1,38 +1,42 @@
+#!/bin/sh
+set -e
 echo "starting time: `date`"
+
 cd jni/deltachat-core-rust
 
 # to setup the toolchains (from https://medium.com/visly/rust-on-android-19f34a2fb43 )
 # run the following in `jni/deltachat-core-rust`:
 # $ rustup target add armv7-linux-androideabi aarch64-linux-android  i686-linux-android x86_64-linux-android
-#
-# then:
-# $ mkdir ~/.NDK
-# $ $ANDROID_NDK/build/tools/make_standalone_toolchain.py --api 14 --arch arm --install-dir ~/.NDK/arm
-# $ $ANDROID_NDK/build/tools/make_standalone_toolchain.py --api 21 --arch arm64 --install-dir ~/.NDK/arm64
-# $ $ANDROID_NDK/build/tools/make_standalone_toolchain.py --api 14 --arch x86 --install-dir ~/.NDK/x86
-# $ $ANDROID_NDK/build/tools/make_standalone_toolchain.py --api 21 --arch x86_64 --install-dir ~/.NDK/x86_64
-# (--api should be aligned with APP_PLATFORM from Application.mk and with minSdkVersion)
-# this installs toolchains in `~/.NDK`
+# after that, add PATH_TO_NDK/toolchains/llvm/prebuilt/HOST/bin to your $PATH
+# and add the correct clang-linkers to `~/.cargo/config`:
+# ```
+# [target.armv7-linux-androideabi]
+# linker = "PATH_TO_NDK/toolchains/llvm/prebuilt/HOST/bin/armv7a-linux-androideabi21-clang"
+# [target.aarch64-linux-android]
+# linker = "PATH_TO_NDK/toolchains/llvm/prebuilt/HOST/bin/aarch64-linux-android21-clang"
+# [target.i686-linux-android]
+# linker = "PATH_TO_NDK/toolchains/llvm/prebuilt/HOST/bin/i686-linux-android21-clang"
+# [target.x86_64-linux-android]
+# linker = "PATH_TO_NDK/toolchains/llvm/prebuilt/HOST/bin/x86_64-linux-android21-clang"
+# ```
 # then, the following should work:
 
+export CFLAGS=-D__ANDROID_API__=21
+
 echo "-- cross compiling to armv7-linux-androideabi (arm) --"
-export RUSTFLAGS="-C linker=$HOME/.NDK/arm/bin/clang";
-export PATH=~/.NDK/arm/bin:$PATH;
+TARGET_CC=armv7a-linux-androideabi21-clang \
 cargo build --release --target armv7-linux-androideabi -p deltachat_ffi
 
 echo "-- cross compiling to aarch64-linux-android (arm64) --"
-export RUSTFLAGS="-C linker=$HOME/.NDK/arm64/bin/clang";
-export PATH=~/.NDK/arm64/bin:$PATH;
+TARGET_CC=aarch64-linux-android21-clang \
 cargo build --release --target aarch64-linux-android -p deltachat_ffi
 
 echo "-- cross compiling to i686-linux-android (x86) --"
-export RUSTFLAGS="-C linker=$HOME/.NDK/x86/bin/clang";
-export PATH=~/.NDK/x86/bin:$PATH;
+TARGET_CC=i686-linux-android21-clang \
 cargo build --release --target i686-linux-android -p deltachat_ffi
 
 echo "-- cross compiling to x86_64-linux-android (x86_64) --"
-export RUSTFLAGS="-C linker=$HOME/.NDK/x86_64/bin/clang";
-export PATH=~/.NDK/x86_64/bin:$PATH;
+TARGET_CC=x86_64-linux-android21-clang \
 cargo build --release --target x86_64-linux-android -p deltachat_ffi
 
 echo -- copy generated .a files --
