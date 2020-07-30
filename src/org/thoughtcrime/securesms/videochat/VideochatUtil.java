78bcd02af61c1c0756f822c528c63768275c8e9b
B. Petersen <r10s@b44t.com>
2020-07-30 23:05:42 +0200

    open videochats as created

--- src/org/thoughtcrime/securesms/videochat/VideochatUtil.java
+++ src/org/thoughtcrime/securesms/videochat/VideochatUtil.java
@@ -23,7 +23,10 @@ public class VideochatUtil {
             .setMessage("This requires a compatible app or a compatible browser on both ends.")
             .setNegativeButton(R.string.cancel, null)
             .setPositiveButton(R.string.ok, (dialog, which) -> {
-                dcContext.sendVideochatInvitation(dcChat.getId());
+                int msgId = dcContext.sendVideochatInvitation(dcChat.getId());
+                if (msgId != 0) {
+                  join(activity, msgId);
+                }
             })
             .show();
   }
