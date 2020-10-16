befe953b6184dab9b05e72b4afaceaadb3e19c1b
Hocuri <hocuri@gmx.de>
2020-10-16 17:14:07 +0200

    formatting

--- src/com/b44t/messenger/DcMsg.java
+++ src/com/b44t/messenger/DcMsg.java
@@ -80,9 +80,9 @@ public class DcMsg {
         int msgs[] = dcContext.getChatMsgs(msg.getChatId(), 0, 0);
         int startingPosition = -1;
         int msgId = msg.getId();
-        for(int i=0; i< msgs.length; i++ ) {
-            if(msgs[i] == msgId) {
-                startingPosition = msgs.length-1-i;
+        for (int i = 0; i < msgs.length; i++) {
+            if (msgs[i] == msgId) {
+                startingPosition = msgs.length - 1 - i;
                 break;
             }
         }
