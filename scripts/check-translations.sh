f99d9daa49d8e76044074fbee4a25fe2d4e10c4f
B. Petersen <r10s@b44t.com>
2020-12-01 11:44:25 +0100

    update translations, add a fix for unescaped '&' entity

--- scripts/check-translations.sh
+++ scripts/check-translations.sh
@@ -11,3 +11,6 @@ grep --include='strings.xml' -r ' \$' .
 # - allow only `\n`, `\"`, `\'` and `\’`
 #   (`’` might not be escaped, but it is done often eg. in "sq", so we allow that for now)
 grep --include='strings.xml' -r "\\\\[^n\"'’]" .
+
+# check for usage of a single `&` - this has to be an `&amp;`
+grep --include='strings.xml' -r "&[^a]" .
