423e287ac5a3c4ba0f1b18396c33fa2644ab6b2d
B. Petersen <r10s@b44t.com>
2021-04-18 14:24:12 +0200

    speedup global search
    the speedup is done by the following changes:
    
    - if the query-string is updated while actually performing a search,
      the new search is not directly scheduled as before;
      instead the flag needsAnotherBgSearch is set;
      in the search-thread, the flag is checked
      and another search will be performed
    
      this way, searches as skipped, but the most recent is always executed
    
    - if there are already chat- or contact-results and
      the query-search has changed meanwhile, search is aborted
      and the partial result is used (next search will be exexuted immediately)
    
      this way, esp. the expensive searchMsgs() search is executed less often
      on fast typing
    
    - for a one-letter-search, search for messages is always skipped
    
    this is roughly what we are doing on ios since
    https://github.com/deltachat/deltachat-ios/pull/1140
    
    moreover, this pr cleans up some things,
    putting logic to the same java class.

--- src/org/thoughtcrime/securesms/search/SearchRepository.java
+++ src/org/thoughtcrime/securesms/search/SearchRepository.java
@@ -1,69 +1,10 @@
 package org.thoughtcrime.securesms.search;
 
-import android.content.Context;
-import android.database.DatabaseUtils;
 import androidx.annotation.NonNull;
-import android.text.TextUtils;
-import android.util.Log;
 
-import com.b44t.messenger.DcChatlist;
-import com.b44t.messenger.DcContext;
-
-
-import org.thoughtcrime.securesms.connect.ApplicationDcContext;
-import org.thoughtcrime.securesms.connect.DcHelper;
 import org.thoughtcrime.securesms.search.model.SearchResult;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.Executor;
-
-/**
- * Manages data retrieval for search.
- */
 class SearchRepository {
-  private static final String        TAG = SearchRepository.class.getSimpleName();
-  private final ApplicationDcContext dcContext;
-  private final Executor             executor;
-  private boolean                    queryMessages = true;
-
-  SearchRepository(@NonNull Context          context,
-                   @NonNull Executor         executor)
-  {
-    this.dcContext        = DcHelper.getContext(context.getApplicationContext());
-    this.executor         = executor;
-  }
-
-  void query(@NonNull String query, @NonNull Callback callback) {
-    if (TextUtils.isEmpty(query)) {
-      callback.onResult(SearchResult.EMPTY);
-      return;
-    }
-
-    executor.execute(() -> {
-      long startMs = System.currentTimeMillis();
-      DcChatlist conversations = dcContext.getChatlist(0, query, 0);
-      Log.i(TAG, "⏰ getChatlist: " + (System.currentTimeMillis() - startMs) + "ms");
-
-      startMs = System.currentTimeMillis();
-      int[]      contacts      = dcContext.getContacts(DcContext.DC_GCL_ADD_SELF, query);
-      Log.i(TAG, "⏰ getContacts: " + (System.currentTimeMillis() - startMs) + "ms");
-
-      int[]      messages      = new int[0];
-      if (queryMessages) {
-        startMs = System.currentTimeMillis();
-        messages = dcContext.searchMsgs(0, query);
-        Log.i(TAG, "⏰ searchMsgs: " + (System.currentTimeMillis() - startMs) + "ms");
-      }
-
-      callback.onResult(new SearchResult(query, contacts, conversations, messages));
-    });
-  }
-
-  public void setQueryMessages(boolean includeMessageQueries) {
-    this.queryMessages = includeMessageQueries;
-  }
-
   public interface Callback {
     void onResult(@NonNull SearchResult result);
   }
