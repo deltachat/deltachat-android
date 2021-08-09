c5312db06b60a6c695c2e3c03b4f922baf45ffc8
Hocuri <hocuri@gmx.de>
2021-08-09 17:17:57 +0200

    Small improvements

--- androidTest/com/b44t/messenger/HelloWorldEspressoTest.java
+++ androidTest/com/b44t/messenger/HelloWorldEspressoTest.java
@@ -4,18 +4,12 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import android.view.View;
 
-import androidx.test.espresso.PerformException;
-import androidx.test.espresso.UiController;
-import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.util.HumanReadables;
-import androidx.test.espresso.util.TreeIterables;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
-import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
@@ -26,24 +20,17 @@ import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.connect.AccountManager;
 import org.thoughtcrime.securesms.connect.DcHelper;
 import org.thoughtcrime.securesms.util.Prefs;
-import androidx.test.espresso.contrib.RecyclerViewActions;
-
-import java.util.concurrent.TimeoutException;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
-
 
 
 @RunWith(AndroidJUnit4.class)
@@ -54,6 +41,11 @@ public class HelloWorldEspressoTest {
   // Set this to true if you already have at least 10 chats on your existing DeltaChat installation
   // and want to traverse through them instead of 10 newly created chats
   private final static boolean USE_EXISTING_CHATS = false;
+  // ==============================================================================================
+  private final static int GO_THROUGH_ALL_CHATS_N_TIMES = 8;
+
+  // ==============================================================================================
+  // PLEASE BACKUP YOUR ACCOUNT BEFORE RUNNING THIS!
   // ==============================================================================================
 
   private final static String TAG = HelloWorldEspressoTest.class.getSimpleName();
@@ -72,7 +64,7 @@ public class HelloWorldEspressoTest {
   }
 
   @Test
-  public void createAndEnter10Chats() {
+  public void createAndEnterNChats() {
     Prefs.setEnterSendsEnabled(getInstrumentation().getTargetContext(), true);
 
     if (!USE_EXISTING_CHATS) {
@@ -81,29 +73,27 @@ public class HelloWorldEspressoTest {
       createChatAndGoBack("Group #3", repeat("Some string ", 600), repeat("Another string", 200), "Hi!!!");
       createChatAndGoBack("Group #4", "xyzabc", "Hi!!!!", "Let's meet!");
       createChatAndGoBack("Group #5", repeat("aaaa", 40), "bbbbbbbbbbbbbbbbbb", "ccccccccccccccc");
-      createChatAndGoBack("Group #6", "aaaaaaaaaaa", repeat("Hi! ", 100), "bbbbbbbbbb");
+      createChatAndGoBack("Group #6", "aaaaaaaaaaa", repeat("Hi! ", 1000), "bbbbbbbbbb");
       createChatAndGoBack("Group #7", repeat("abcdefg ", 500), repeat("xxxxx", 100), "yrrrrrrrrrrrrr");
       createChatAndGoBack("Group #8", "and a number: 037362/384756", "ccccc", "Nice!");
       createChatAndGoBack("Group #9", "ddddddddddddddddd", "zuuuuuuuuuuuuuuuu", "ccccc");
       createChatAndGoBack("Group #10", repeat("xxxxxxyyyyy", 100), repeat("String!!", 10), "abcd");
     }
 
-    String[] times = new String[10];
-    for (int i = 0; i<10; i++) {
+    String[] times = new String[GO_THROUGH_ALL_CHATS_N_TIMES];
+    for (int i = 0; i<GO_THROUGH_ALL_CHATS_N_TIMES; i++) {
       times[i] = "" + timeGoToAllChats();
     }
-    Log.i(TAG, "MEASURED RESULTS (Benchmark): " + String.join(",", times));
+    Log.i(TAG, "MEASURED RESULTS (Benchmark) - Going thorough all 10 chats: " + String.join(",", times));
   }
 
   private long timeGoToAllChats() {
     long start = System.currentTimeMillis();
     for (int i=0; i<10; i++) {
       onView(withId(R.id.list)).perform(RecyclerViewActions.actionOnItemAtPosition(i, click()));
-
       pressBack();
     }
-    long end = System.currentTimeMillis();
-    long diff = end - start;
+    long diff = System.currentTimeMillis() - start;
     Log.i(TAG, "Measured (Benchmark): Going through all chats took " + diff + "ms");
     return diff;
   }
@@ -139,12 +129,12 @@ public class HelloWorldEspressoTest {
 
   @After
   public void removeMockAccount() {
-    Context context = getInstrumentation().getTargetContext();
-    DcAccounts accounts = DcHelper.getAccounts(context);
+    if (!USE_EXISTING_CHATS) {
+      Context context = getInstrumentation().getTargetContext();
+      DcAccounts accounts = DcHelper.getAccounts(context);
 
-    DcContext selectedAccount = accounts.getSelectedAccount();
-    accounts.removeAccount(selectedAccount.getAccountId());
-
-    AccountManager.getInstance().switchAccount(context, selectedAccount.getAccountId());
+      DcContext selectedAccount = accounts.getSelectedAccount();
+      accounts.removeAccount(selectedAccount.getAccountId());
+    }
   }
 }
