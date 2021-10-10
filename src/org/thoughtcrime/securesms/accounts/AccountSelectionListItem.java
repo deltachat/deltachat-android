746c666148903298226590b3b87336bca4eaafc0
adbenitez <asieldbenitez@gmail.com>
2021-10-10 02:05:58 -0400

    improve fallback avatar in account list

--- src/org/thoughtcrime/securesms/accounts/AccountSelectionListItem.java
+++ src/org/thoughtcrime/securesms/accounts/AccountSelectionListItem.java
@@ -75,7 +75,7 @@ public class AccountSelectionListItem extends LinearLayout {
     Recipient recipient;
     if (accountId != DcContact.DC_CONTACT_ID_ADD_ACCOUNT) {
       deleteBtn.setVisibility(View.VISIBLE);
-      recipient = new Recipient(getContext(), self);
+      recipient = new Recipient(getContext(), self, name);
     } else {
       deleteBtn.setVisibility(View.GONE);
       recipient = null;
