a8f61ee23bf29d60bb595711f7bece5df5be77c9
cyberta <cyberta@riseup.net>
2021-11-03 21:43:29 +0100

    little clean-up

--- src/org/thoughtcrime/securesms/service/IPCAddAccountsService.java
+++ src/org/thoughtcrime/securesms/service/IPCAddAccountsService.java
@@ -20,6 +20,10 @@ import java.lang.ref.WeakReference;
 
 import static org.thoughtcrime.securesms.connect.DcHelper.CONFIG_ADDRESS;
 
+/**
+ * This service is invoked by companion apps aiming to add a new account to Delta Chat
+ */
+
 public class IPCAddAccountsService extends Service {
   public final static int ADD_ACCOUNT = 1;
   public final static String ACCOUNT_DATA = "ACCOUNT_DATA";
@@ -35,7 +39,6 @@ public class IPCAddAccountsService extends Service {
 
     @Override
     public void handleMessage(@NonNull Message msg) {
-      Log.d(TAG, "handle Message");
       Bundle data = msg.getData();
       Context context = contextRef.get();
       if (data != null && context != null && msg.what == ADD_ACCOUNT) {
@@ -53,26 +56,6 @@ public class IPCAddAccountsService extends Service {
 
   private final Messenger messenger = new Messenger(new IncomingHandler(this));
 
-  /**
-   * Return the communication channel to the service.  May return null if
-   * clients can not bind to the service.  The returned
-   * {@link IBinder} is usually for a complex interface
-   * that has been <a href="{@docRoot}guide/components/aidl.html">described using
-   * aidl</a>.
-   *
-   * <p><em>Note that unlike other application components, calls on to the
-   * IBinder interface returned here may not happen on the main thread
-   * of the process</em>.  More information about the main thread can be found in
-   * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html">Processes and
-   * Threads</a>.</p>
-   *
-   * @param intent The Intent that was used to bind to this service,
-   *               as given to {@link Context#bindService
-   *               Context.bindService}.  Note that any extras that were included with
-   *               the Intent at that point will <em>not</em> be seen here.
-   * @return Return an IBinder through which clients can call on to the
-   * service.
-   */
   @Nullable
   @Override
   public IBinder onBind(Intent intent) {
