d83346c689e8879c4adf5fa6f85dcf8eaf6e01a7
adbenitez <asieldbenitez@gmail.com>
2021-11-12 01:14:18 -0500

    configure default webrtc instance

--- src/org/thoughtcrime/securesms/ConversationListActivity.java
+++ src/org/thoughtcrime/securesms/ConversationListActivity.java
@@ -102,7 +102,7 @@ public class ConversationListActivity extends PassphraseRequiredActionBarActivit
 
     msg = new DcMsg(dcContext, DcMsg.DC_MSG_TEXT);
     msg.setText(getString(R.string.update_1_24_android) + " https://delta.chat/en/blog");
-    dcContext.addDeviceMsg("update_1_24_android", msg); // addDeviceMessage() makes sure, messages with the same id are not added twice
+    dcContext.addDeviceMsg("update_1_24a_android", msg); // addDeviceMessage() makes sure, messages with the same id are not added twice
 
     // create view
     setContentView(R.layout.conversation_list_activity);
