b0db45e5a1c287d643554c8fa7868c19d53c5ab3
B. Petersen <r10s@b44t.com>
2022-01-31 22:40:36 +0100

    add 1.28 device message

--- src/org/thoughtcrime/securesms/ConversationListActivity.java
+++ src/org/thoughtcrime/securesms/ConversationListActivity.java
@@ -100,12 +100,8 @@ public class ConversationListActivity extends PassphraseRequiredActionBarActivit
     // it is not needed to keep all past update messages, however, when deleted, also the strings should be deleted.
     DcContext dcContext = DcHelper.getContext(this);
     DcMsg msg = new DcMsg(dcContext, DcMsg.DC_MSG_TEXT);
-    msg.setText(getString(R.string.update_1_24_android) + " https://delta.chat/en/blog");
-    dcContext.addDeviceMsg("update_1_24n_android", msg); // addDeviceMessage() makes sure, messages with the same id are not added twice
-
-    msg = new DcMsg(dcContext, DcMsg.DC_MSG_TEXT);
-    msg.setText(getString(R.string.update_1_26));
-    dcContext.addDeviceMsg("update_1_26c_android", msg); // addDeviceMessage() makes sure, messages with the same id are not added twice
+    msg.setText(getString(R.string.update_1_28_android));
+    dcContext.addDeviceMsg("update_1_28e_android", msg); // addDeviceMessage() makes sure, messages with the same id are not added twice
 
     // create view
     setContentView(R.layout.conversation_list_activity);
