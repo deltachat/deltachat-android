f047f1382ed681a75c5cb1c488fc6fe4faaf10bf
B. Petersen <r10s@b44t.com>
2022-04-23 14:13:59 +0200

    add resendMsgs() api

--- jni/dc_wrapper.c
+++ jni/dc_wrapper.c
@@ -711,6 +711,16 @@ JNIEXPORT void Java_com_b44t_messenger_DcContext_forwardMsgs(JNIEnv *env, jobjec
 }
 
 
+JNIEXPORT jboolean Java_com_b44t_messenger_DcContext_resendMsgs(JNIEnv *env, jobject obj, jintArray msg_ids)
+{
+    int msg_ids_cnt = 0;
+    uint32_t* msg_ids_ptr = jintArray2uint32Pointer(env, msg_ids, &msg_ids_cnt);
+        jboolean ret = dc_resend_msgs(get_dc_context(env, obj), msg_ids_ptr, msg_ids_cnt) != 0;
+    free(msg_ids_ptr);
+    return ret;
+}
+
+
 JNIEXPORT jint Java_com_b44t_messenger_DcContext_prepareMsg(JNIEnv *env, jobject obj, jint chat_id, jobject msg)
 {
     return dc_prepare_msg(get_dc_context(env, obj), chat_id, get_dc_msg(env, msg));
