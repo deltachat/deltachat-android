1b991bee2ec82ab01430f3f7b71fb5d43f3e12dd
B. Petersen <r10s@b44t.com>
2023-03-17 21:05:47 +0100

    use our base activity

--- src/org/thoughtcrime/securesms/qr/BackupTransferActivity.java
+++ src/org/thoughtcrime/securesms/qr/BackupTransferActivity.java
@@ -2,18 +2,19 @@ package org.thoughtcrime.securesms.qr;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 
+import org.thoughtcrime.securesms.PassphraseRequiredActionBarActivity;
 import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.service.GenericForegroundService;
 import org.thoughtcrime.securesms.service.NotificationController;
 import org.thoughtcrime.securesms.util.DynamicLanguage;
 import org.thoughtcrime.securesms.util.DynamicTheme;
 
-public class BackupTransferActivity extends AppCompatActivity {
+public class BackupTransferActivity extends PassphraseRequiredActionBarActivity {
 
     public enum TransferMode {
         INVALID(0),
@@ -42,19 +43,30 @@ public class BackupTransferActivity extends AppCompatActivity {
     NotificationController notificationController;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onPreCreate() {
         dynamicTheme.onCreate(this);
         dynamicLanguage.onCreate(this);
+    }
+
+    @Override
+    protected void onCreate(Bundle icicle, boolean ready) {
+        super.onCreate(icicle, ready);
 
         transferMode = TransferMode.fromInt(getIntent().getIntExtra(TRANSFER_MODE, TransferMode.INVALID.getInt()));
-        if (transferMode == TransferMode.INVALID) {
-          throw new RuntimeException("invalid transfer mode");
+        if (transferMode != TransferMode.INVALID) {
+            Toast.makeText(this, "bad transfer mode", Toast.LENGTH_LONG).show();
+            finish();
         }
 
         notificationController = GenericForegroundService.startForegroundTask(this, getString(R.string.multidevice_title));
 
         setContentView(R.layout.backup_provider_activity);
+
+        switch(transferMode) {
+            case SENDER_SHOW_QR:
+                initFragment(android.R.id.content, new BackupProviderFragment(), dynamicLanguage.getCurrentLocale(), icicle);
+                break;
+        }
 
         ActionBar supportActionBar = getSupportActionBar();
         supportActionBar.setDisplayHomeAsUpEnabled(true);
