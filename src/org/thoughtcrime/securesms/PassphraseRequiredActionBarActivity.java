a6ea240c265971c59260d452e6961fb1ac9823f5
bjoern <r10s@b44t.com>
2023-03-25 00:12:17 +0100

    cleanup base activities (#2508)
    * remove superfluous overwrites
    
    * move initFragment() to BaseActivity so that it can be better accessed

--- src/org/thoughtcrime/securesms/PassphraseRequiredActionBarActivity.java
+++ src/org/thoughtcrime/securesms/PassphraseRequiredActionBarActivity.java
@@ -4,20 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.annotation.IdRes;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import org.thoughtcrime.securesms.connect.DcHelper;
 import org.thoughtcrime.securesms.service.GenericForegroundService;
 
-import java.util.Locale;
-
 public abstract class PassphraseRequiredActionBarActivity extends BaseActionBarActivity {
   private static final String TAG = PassphraseRequiredActionBarActivity.class.getSimpleName();
-
-  public static final String LOCALE_EXTRA = "locale_extra";
 
   @Override
   protected final void onCreate(Bundle savedInstanceState) {
@@ -48,54 +39,4 @@ public abstract class PassphraseRequiredActionBarActivity extends BaseActionBarA
 
   protected void onPreCreate() {}
   protected void onCreate(Bundle savedInstanceState, boolean ready) {}
-
-  @Override
-  protected void onResume() {
-    Log.i(TAG, "onResume()");
-    super.onResume();
-  }
-
-  @Override
-  protected void onPause() {
-    Log.i(TAG, "onPause()");
-    super.onPause();
-  }
-
-  @Override
-  protected void onDestroy() {
-    Log.i(TAG, "onDestroy()");
-    super.onDestroy();
-  }
-
-  protected <T extends Fragment> T initFragment(@IdRes int target,
-                                                @NonNull T fragment)
-  {
-    return initFragment(target, fragment, null);
-  }
-
-  protected <T extends Fragment> T initFragment(@IdRes int target,
-                                                @NonNull T fragment,
-                                                @Nullable Locale locale)
-  {
-    return initFragment(target, fragment, locale, null);
-  }
-
-  protected <T extends Fragment> T initFragment(@IdRes int target,
-                                                @NonNull T fragment,
-                                                @Nullable Locale locale,
-                                                @Nullable Bundle extras)
-  {
-    Bundle args = new Bundle();
-    args.putSerializable(LOCALE_EXTRA, locale);
-
-    if (extras != null) {
-      args.putAll(extras);
-    }
-
-    fragment.setArguments(args);
-    getSupportFragmentManager().beginTransaction()
-                               .replace(target, fragment)
-                               .commitAllowingStateLoss();
-    return fragment;
-  }
 }
