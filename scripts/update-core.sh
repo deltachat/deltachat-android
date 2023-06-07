5df31105ce2157d259a76dacdfe5f72de41d6e50
bjoern <r10s@b44t.com>
2023-06-07 11:50:59 +0200

    ./update-core.sh accepts tags as well as branches (#2583)
    * ./update-core.sh accepts tags as well as branches
    
    * stop update.core.sh on all errors

--- scripts/update-core.sh
+++ scripts/update-core.sh
@@ -1,29 +1,29 @@
+set -e # stop on all errors
+
 git submodule update --init --recursive
 cd jni/deltachat-core-rust
 OLD=`git branch --show-current`
 if [ $# -eq 0 ]; then
-    echo "updates deltachat-core-rust submodule to last commit of a branch."
-    echo "usage: ./scripts/update-core.sh BRANCH_NAME"
+    echo "updates deltachat-core-rust submodule to a tag or to last commit of a branch."
+    echo "usage: ./scripts/update-core.sh BRANCH_OR_TAG"
     echo "current branch: $OLD"
     exit
 fi
-BRANCH=$1
-
+NEW=$1
 
 git fetch
-git checkout $BRANCH
+git checkout $NEW
 TEST=`git branch --show-current`
-if [ "$TEST" != "$BRANCH" ]; then
-    echo "cannot select branch: $BRANCH"
-    exit
+if [ "$TEST" == "$NEW" ]; then
+    git pull
 fi
-git pull
+
 commitmsg=`git log -1 --pretty=%s`
 cd ../..
 
 
 git add jni/deltachat-core-rust
-git commit -m "update deltachat-core-rust to '$commitmsg' of branch '$BRANCH'"
-echo "old branch: $OLD, new branch: $BRANCH"
+git commit -m "update deltachat-core-rust to '$commitmsg' of '$NEW'"
+echo "old: $OLD, new: $NEW"
 echo "changes are committed to local repo."
 echo "use 'git push' to use them or 'git reset HEAD~1; git submodule update --recursive' to abort."
