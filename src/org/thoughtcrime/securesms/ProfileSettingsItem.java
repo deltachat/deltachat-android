ded7f5b80581fcb4a6777f339ba3a8485884afe5
B. Petersen <r10s@b44t.com>
2023-09-26 00:46:32 +0200

    make the button look more like a button

--- src/org/thoughtcrime/securesms/ProfileSettingsItem.java
+++ src/org/thoughtcrime/securesms/ProfileSettingsItem.java
@@ -5,6 +5,10 @@ import android.util.AttributeSet;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.core.content.ContextCompat;
+
+import org.thoughtcrime.securesms.util.ResUtil;
+
 public class ProfileSettingsItem extends LinearLayout {
 
   private TextView labelView;
@@ -23,8 +27,15 @@ public class ProfileSettingsItem extends LinearLayout {
     labelView = findViewById(R.id.label);
   }
 
-  public void set(String label, int iconLeft) {
+  public void set(String label, int labelColor, int iconLeft) {
     labelView.setText(label==null? "" : label);
     labelView.setCompoundDrawablesWithIntrinsicBounds(iconLeft, 0,0,0);
+
+    // we need different color getters as `labelColor` is `R.color.name` while default is `R.attr.name`
+    if (labelColor != 0) {
+      labelView.setTextColor(ContextCompat.getColor(getContext(), labelColor));
+    } else {
+      labelView.setTextColor(ResUtil.getColor(getContext(), R.attr.emoji_text_color)); //
+    }
   }
 }
