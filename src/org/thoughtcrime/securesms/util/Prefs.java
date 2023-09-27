30d4febee78c4000823f45a6aaa1c86736e3fd76
B. Petersen <r10s@b44t.com>
2023-09-27 18:07:06 +0200

    use system camera as default
    the system camera has usually more features (flash, physical zoom, filter)
    and is less buggy than the mostly unmaintained built-in one.
    also, the system camera seems to be more supported
    (isDeviceSupported() is not needed) and does not require to ask for permission.
    
    there is still a switch in "advanced settings" to use the old built-in camera,
    however, at some point we may remove the old code completely.

--- src/org/thoughtcrime/securesms/util/Prefs.java
+++ src/org/thoughtcrime/securesms/util/Prefs.java
@@ -56,6 +56,7 @@ public class Prefs {
   public  static final String NOTIFICATION_PRIORITY_PREF       = "pref_notification_priority";
 
   public  static final String SYSTEM_EMOJI_PREF                = "pref_system_emoji";
+  public  static final String BUILTIN_CAMERA_PREF              = "pref_builtin_camera";
   public  static final String DIRECT_CAPTURE_CAMERA_ID         = "pref_direct_capture_camera_id";
   private static final String PROFILE_AVATAR_ID_PREF           = "pref_profile_avatar_id";
   public  static final String INCOGNITO_KEYBORAD_PREF          = "pref_incognito_keyboard";
@@ -314,6 +315,10 @@ public class Prefs {
 
   public static boolean isSystemEmojiPreferred(Context context) {
     return getBooleanPreference(context, SYSTEM_EMOJI_PREF, false);
+  }
+
+  public static boolean isBuiltInCameraPreferred(Context context) {
+    return getBooleanPreference(context, BUILTIN_CAMERA_PREF, false);
   }
 
   public static boolean getAlwaysLoadRemoteContent(Context context) {
