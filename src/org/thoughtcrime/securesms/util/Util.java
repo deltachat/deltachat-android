7ccf26d0e70212d11d3b7f15ba61631c55111c42
adbenitez <asieldbenitez@gmail.com>
2023-09-30 22:45:30 +0200

    remove unnecessary copyToClipboardSdk11()

--- src/org/thoughtcrime/securesms/util/Util.java
+++ src/org/thoughtcrime/securesms/util/Util.java
@@ -269,18 +269,6 @@ public class Util {
   }
 
   public static void writeTextToClipboard(@NonNull Context context, @NonNull String text) {
-    int sdk = android.os.Build.VERSION.SDK_INT;
-    if (sdk < android.os.Build.VERSION_CODES.HONEYCOMB) {
-      @SuppressWarnings("deprecation") android.text.ClipboardManager clipboard =
-              (android.text.ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-      clipboard.setText(text);
-    } else {
-      copyToClipboardSdk11(context, text);
-    }
-  }
-
-  @TargetApi(android.os.Build.VERSION_CODES.HONEYCOMB)
-  private static void copyToClipboardSdk11(Context context, String text) {
     android.content.ClipboardManager clipboard =
             (android.content.ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
     ClipData clip = ClipData.newPlainText(context.getString(R.string.app_name), text);
