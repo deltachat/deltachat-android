9ffed8c32d834d51586a50b6811c2b61cdc8b994
B. Petersen <r10s@b44t.com>
2023-10-06 16:09:41 +0200

    detect self-reaction

--- src/org/thoughtcrime/securesms/reactions/AddReactionView.java
+++ src/org/thoughtcrime/securesms/reactions/AddReactionView.java
@@ -5,15 +5,19 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.b44t.messenger.DcContact;
 import com.b44t.messenger.DcContext;
+import com.b44t.messenger.rpc.Reactions;
 import com.b44t.messenger.rpc.Rpc;
 
 import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.components.emoji.EmojiTextView;
 import org.thoughtcrime.securesms.connect.DcHelper;
 
+import java.util.Map;
+
 public class AddReactionView extends LinearLayout {
-    private EmojiTextView [] defaultReactions;
+    private EmojiTextView [] defaultReactionViews;
     private Context context;
     private DcContext dcContext;
     private Rpc rpc;
@@ -33,16 +37,16 @@ public class AddReactionView extends LinearLayout {
           context = getContext();
           dcContext = DcHelper.getContext(context);
           rpc = DcHelper.getRpc(getContext());
-          defaultReactions = new EmojiTextView[]{
+          defaultReactionViews = new EmojiTextView[]{
               findViewById(R.id.reaction_0),
               findViewById(R.id.reaction_1),
               findViewById(R.id.reaction_2),
               findViewById(R.id.reaction_3),
               findViewById(R.id.reaction_4),
           };
-          for (int i = 0; i < defaultReactions.length; i++) {
+          for (int i = 0; i < defaultReactionViews.length; i++) {
               final int ii = i;
-              defaultReactions[i].setOnClickListener(v -> reactionClicked(ii));
+              defaultReactionViews[i].setOnClickListener(v -> reactionClicked(ii));
           }
       }
     }
@@ -51,6 +55,14 @@ public class AddReactionView extends LinearLayout {
         init(); // init delayed as needed
         this.msgIdToReactTo = msgIdToReactTo;
         this.listener = listener;
+
+        final String existingReaction = getSelfReaction();
+        for (EmojiTextView defaultReactionView : defaultReactionViews) {
+            if (defaultReactionView.getText().toString().equals(existingReaction)) {
+                // TODO: hilite
+            }
+        }
+
         setVisibility(View.VISIBLE);
     }
 
@@ -58,9 +70,24 @@ public class AddReactionView extends LinearLayout {
         setVisibility(View.GONE);
     }
 
+    private String getSelfReaction() {
+        String result = null;
+        try {
+            final Reactions reactions = rpc.getMsgReactions(dcContext.getAccountId(), msgIdToReactTo);
+            final Map<Integer, String[]> reactionsByContact = reactions.getReactionsByContact();
+            final String [] selfReactions = reactionsByContact.get(DcContact.DC_CONTACT_ID_SELF);
+            if (selfReactions != null && selfReactions.length > 0) {
+                result = selfReactions[0];
+            }
+        } catch(Exception e) {
+           e.printStackTrace();
+        }
+        return result;
+    }
+
     private void reactionClicked(int i) {
         try {
-            final String reaction = defaultReactions[i].getText().toString();
+            final String reaction = defaultReactionViews[i].getText().toString();
             rpc.sendReaction(dcContext.getAccountId(), msgIdToReactTo, reaction);
             if (listener != null) {
                 listener.onShallHide();
