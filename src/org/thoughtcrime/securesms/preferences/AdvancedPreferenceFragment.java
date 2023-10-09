d18ead79dab1342f03c265e6fbcfe97ec0322637
B. Petersen <r10s@b44t.com>
2023-10-09 23:50:29 +0200

    use manageKeys() as name for the import/export keys function (exportKeys() is misleading)

--- src/org/thoughtcrime/securesms/preferences/AdvancedPreferenceFragment.java
+++ src/org/thoughtcrime/securesms/preferences/AdvancedPreferenceFragment.java
@@ -191,7 +191,7 @@ public class AdvancedPreferenceFragment extends ListSummaryPreferenceFragment
     super.onActivityResult(requestCode, resultCode, data);
     if (resultCode != RESULT_OK) return;
     if (requestCode == REQUEST_CODE_CONFIRM_CREDENTIALS_KEYS) {
-        exportKeys();
+        manageKeys();
     } else if (requestCode == PICK_SELF_KEYS) {
         Uri uri = (data != null ? data.getData() : null);
         if (uri == null) {
@@ -344,13 +344,13 @@ public class AdvancedPreferenceFragment extends ListSummaryPreferenceFragment
     public boolean onPreferenceClick(Preference preference) {
       boolean result = ScreenLockUtil.applyScreenLock(getActivity(), getString(R.string.pref_manage_keys), getString(R.string.enter_system_secret_to_continue), REQUEST_CODE_CONFIRM_CREDENTIALS_KEYS);
       if (!result) {
-        exportKeys();
+        manageKeys();
       }
       return true;
     }
   }
 
-  private void exportKeys() {
+  private void manageKeys() {
     Permissions.with(getActivity())
         .request(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
         .alwaysGrantOnSdk33()
