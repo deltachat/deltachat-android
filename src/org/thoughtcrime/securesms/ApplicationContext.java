4de5f7c2cc45793767e5d03edf5e9946ec352673
Hocuri <hocuri@gmx.de>
2023-10-27 16:00:57 +0200

    Revert "Implement feature flag for verified 1:1 chats"
    This reverts commit 2efd17edfcf14e3cf89a6671df38647b552e5661.

--- src/org/thoughtcrime/securesms/ApplicationContext.java
+++ src/org/thoughtcrime/securesms/ApplicationContext.java
@@ -1,5 +1,6 @@
 package org.thoughtcrime.securesms;
 
+import static org.thoughtcrime.securesms.connect.DcHelper.CONFIG_VERIFIED_ONE_ON_ONE_CHATS;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -138,6 +139,10 @@ public class ApplicationContext extends MultiDexApplication {
       Prefs.setStringPreference(this, "pref_chat_background", "");
     }
     // /migrating chat backgrounds
+
+    for (int accountId : allAccounts) {
+      dcAccounts.getAccount(accountId).setConfig(CONFIG_VERIFIED_ONE_ON_ONE_CHATS, "1");
+    }
 
     // set translations before starting I/O to avoid sending untranslated MDNs (issue #2288)
     DcHelper.setStockTranslations(this);
