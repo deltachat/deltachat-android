e57ae0b600fb1fed3db5298b7124f5955e42eeb4
adbenitez <asieldbenitez@gmail.com>
2023-11-01 06:24:58 +0100

    improve group creation screen

--- src/org/thoughtcrime/securesms/GroupCreateActivity.java
+++ src/org/thoughtcrime/securesms/GroupCreateActivity.java
@@ -4,14 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.SparseBooleanArray;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -26,40 +23,30 @@ import androidx.loader.app.LoaderManager;
 import com.b44t.messenger.DcChat;
 import com.b44t.messenger.DcContact;
 import com.b44t.messenger.DcContext;
-import com.b44t.messenger.DcEvent;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.transition.Transition;
 import com.soundcloud.android.crop.Crop;
 
 import org.thoughtcrime.securesms.components.AvatarSelector;
-import org.thoughtcrime.securesms.connect.DcEventCenter;
 import org.thoughtcrime.securesms.connect.DcHelper;
 import org.thoughtcrime.securesms.contacts.avatars.ResourceContactPhoto;
-import org.thoughtcrime.securesms.database.Address;
 import org.thoughtcrime.securesms.mms.AttachmentManager;
 import org.thoughtcrime.securesms.mms.GlideApp;
 import org.thoughtcrime.securesms.profiles.AvatarHelper;
-import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.scribbles.ScribbleActivity;
 import org.thoughtcrime.securesms.util.IntentUtils;
-import org.thoughtcrime.securesms.util.SelectedRecipientsAdapter;
-import org.thoughtcrime.securesms.util.SelectedRecipientsAdapter.OnRecipientDeletedListener;
+import org.thoughtcrime.securesms.util.SelectedContactsAdapter;
+import org.thoughtcrime.securesms.util.SelectedContactsAdapter.ItemClickListener;
 import org.thoughtcrime.securesms.util.ThemeUtil;
 import org.thoughtcrime.securesms.util.ViewUtil;
-import org.thoughtcrime.securesms.util.guava.Optional;
-import org.thoughtcrime.securesms.util.task.ProgressDialogAsyncTask;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-
-import static com.b44t.messenger.DcContact.DC_CONTACT_ID_SELF;
+import java.util.Objects;
 
 public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
-                                 implements OnRecipientDeletedListener, DcEventCenter.DcEventDelegate
+                                 implements ItemClickListener
 {
 
   public static final String EDIT_GROUP_CHAT_ID = "edit_group_chat_id";
@@ -83,7 +70,6 @@ public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
   private boolean      isEdit;
   private boolean      avatarChanged;
   private boolean      imageLoaded;
-  private boolean      hasSuggestedContacts;
   private AttachmentManager attachmentManager;
 
   @Override
@@ -109,12 +95,7 @@ public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
       broadcast = dcChat.isBroadcast();
     }
 
-    initializeExistingGroup();
     initializeResources();
-
-    DcEventCenter eventCenter = DcHelper.getEventCenter(this);
-    eventCenter.addObserver(DcContext.DC_EVENT_CHAT_MODIFIED, this);
-    eventCenter.addObserver(DcContext.DC_EVENT_CONTACTS_CHANGED, this);
   }
 
   @Override
@@ -125,7 +106,6 @@ public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
 
   @Override
   protected void onDestroy() {
-    DcHelper.getEventCenter(this).removeObservers(this);
     super.onDestroy();
   }
 
@@ -140,10 +120,7 @@ public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
     }
     else if(broadcast) {
       title = getString(R.string.new_broadcast_list);
-    } else if (hasSuggestedContacts) {
-      title = getString(R.string.new_group_or_subject);
-    }
-    else if(verified) {
+    } else if(verified) {
       title = getString(R.string.menu_new_verified_group);
     }
     else {
@@ -152,99 +129,31 @@ public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
     getSupportActionBar().setTitle(title);
   }
 
-  private void addSelectedContacts(@NonNull Recipient... recipients) {
-    getAdapter().clear();
-    new AddMembersTask(this).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, recipients);
-  }
-
-  @Override
-  public void handleEvent(@NonNull DcEvent event) {
-    int eventId = event.getId();
-    if (eventId== DcContext.DC_EVENT_CHAT_MODIFIED || eventId==DcContext.DC_EVENT_CONTACTS_CHANGED) {
-      initializeExistingGroup();
-    }
-  }
-
-  private static class AddMembersTask extends AsyncTask<Recipient,Void,List<AddMembersTask.Result>> {
-    static class Result {
-      final Optional<Recipient> recipient;
-      final String              reason;
-
-      public Result(@Nullable Recipient recipient, @Nullable String reason) {
-        this.recipient = Optional.fromNullable(recipient);
-        this.reason    = reason;
-      }
-    }
-
-    private final GroupCreateActivity activity;
-
-    public AddMembersTask(@NonNull GroupCreateActivity activity) {
-      this.activity      = activity;
-    }
-
-    @Override
-    protected List<Result> doInBackground(Recipient... recipients) {
-      final List<Result> results = new LinkedList<>();
-
-      for (Recipient recipient : recipients) {
-        results.add(new Result(recipient, null));
-      }
-      return results;
-    }
-
-    @Override
-    protected void onPostExecute(List<Result> results) {
-      if (activity.isFinishing()) return;
-      for (Result result : results) {
-        Recipient recipient = result.recipient.get();
-        Address address = recipient.getAddress();
-        if(address.isDcContact()) {
-          activity.getAdapter().add(recipient);
-        }
-      }
-      activity.updateViewState();
-    }
-  }
-
   private void initializeResources() {
     lv                  = ViewUtil.findById(this, R.id.selected_contacts_list);
     avatar              = ViewUtil.findById(this, R.id.avatar);
     groupName           = ViewUtil.findById(this, R.id.group_name);
     TextView groupHints = ViewUtil.findById(this, R.id.group_hints);
 
-    List<Recipient> initList = new LinkedList<>();
-    if (!broadcast) {
-      DcContact self = dcContext.getContact(DC_CONTACT_ID_SELF);
-      initList.add(new Recipient(this, self));
-    } else {
-      groupName.setHint(R.string.broadcast_list_name);
-    }
-    ArrayList<Integer> suggestedContactIds = getIntent().getIntegerArrayListExtra(SUGGESTED_CONTACT_IDS);
-    if (suggestedContactIds != null && !suggestedContactIds.isEmpty()) {
-      hasSuggestedContacts = true;
-      for (Integer contactId : suggestedContactIds) {
-        initList.add(new Recipient(this, dcContext.getContact(contactId)));
-      }
-    }
-    SelectedRecipientsAdapter adapter = new SelectedRecipientsAdapter(this, GlideApp.with(this), initList);
-    adapter.setOnRecipientDeletedListener(this);
+    SelectedContactsAdapter adapter = new SelectedContactsAdapter(this, GlideApp.with(this), broadcast);
+    adapter.setItemClickListener(this);
     lv.setAdapter(adapter);
 
-    findViewById(R.id.add_member_button).setOnClickListener(new AddRecipientButtonListener());
+    ArrayList<Integer> suggestedContactIds = getIntent().getIntegerArrayListExtra(SUGGESTED_CONTACT_IDS);
+    adapter.changeData(suggestedContactIds);
     initializeAvatarView();
 
     if (broadcast) {
       avatar.setVisibility(View.GONE);
+      groupName.setHint(R.string.broadcast_list_name);
       groupHints.setText(R.string.chat_new_broadcast_hint);
       groupHints.setVisibility(isEdit()? View.GONE : View.VISIBLE);
-      ((TextView)ViewUtil.findById(this, R.id.add_member_button)).setText(R.string.add_recipients);
     } else if (!verified) {
       groupHints.setVisibility(View.GONE);
     }
 
     if(isEdit()) {
       lv.setVisibility(View.GONE);
-      findViewById(R.id.add_member_button).setVisibility(View.GONE);
     }
   }
 
@@ -270,13 +179,6 @@ public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
     );
   }
 
-  private void initializeExistingGroup() {
-    if (groupChatId != 0) {
-      getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-      new FillExistingGroupInfoAsyncTask(this).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, groupChatId);
-    }
-  }
-
   @Override
   public boolean onPrepareOptionsMenu(Menu menu) {
     MenuInflater inflater = this.getMenuInflater();
@@ -288,7 +190,7 @@ public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
   }
 
   @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
     super.onOptionsItemSelected(item);
     switch (item.getItemId()) {
       case android.R.id.home:
@@ -326,8 +228,8 @@ public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
   }
 
   private boolean allMembersVerified() {
-    for (Recipient recipient : getAdapter().getRecipients()) {
-      DcContact contact = recipient.getDcContact();
+    for (int id : getAdapter().getContacts()) {
+      DcContact contact = dcContext.getContact(id);
       if (contact != null && !contact.isVerified()) {
         return false;
       }
@@ -336,9 +238,22 @@ public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
   }
 
   @Override
-  public void onRecipientDeleted(Recipient recipient) {
-    getAdapter().remove(recipient);
-    updateViewState();
+  public void onItemClick(int contactId) {
+    if (contactId == DcContact.DC_CONTACT_ID_ADD_MEMBER) {
+      Intent intent = new Intent(this, ContactMultiSelectionActivity.class);
+      intent.putExtra(ContactSelectionListFragment.SELECT_VERIFIED_EXTRA, verified);
+      ArrayList<String> preselectedContacts = new ArrayList<>();
+      for (int id : getAdapter().getContacts()) {
+        preselectedContacts.add(dcContext.getContact(id).getAddr());
+      }
+      intent.putExtra(ContactSelectionListFragment.PRESELECTED_CONTACTS, preselectedContacts);
+      startActivityForResult(intent, PICK_CONTACT);
+    }
+  }
+
+  @Override
+  public void onItemDeleteClick(int contactId) {
+    getAdapter().remove(contactId);
   }
 
   private void createGroup(String groupName) {
@@ -349,13 +264,8 @@ public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
       groupChatId = dcContext.createGroupChat(verified, groupName);
     }
 
-    Set<Recipient> recipients = getAdapter().getRecipients();
-    for (Recipient recipient : recipients) {
-      Address address = recipient.getAddress();
-      if (address.isDcContact()) {
-        int contactId = address.getDcContactId();
-        dcContext.addContactToChat(groupChatId, contactId);
-      }
+    for (int contactId : getAdapter().getContacts()) {
+      dcContext.addContactToChat(groupChatId, contactId);
     }
     if (avatarBmp!=null) {
       AvatarHelper.setGroupAvatar(this, groupChatId, avatarBmp);
@@ -381,7 +291,6 @@ public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
       return;
     }
     dcContext.setChatName(groupChatId, groupName);
-    updateGroupParticipants();
 
     if (avatarChanged) AvatarHelper.setGroupAvatar(this, groupChatId, avatarBmp);
 
@@ -392,33 +301,8 @@ public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
     finish();
   }
 
-  private void updateGroupParticipants() {
-    SparseBooleanArray currentChatContactIds = new SparseBooleanArray();
-    for(int chatContactId : dcContext.getChatContacts(groupChatId)) {
-      currentChatContactIds.put(chatContactId, false);
-    }
-
-    Set<Recipient> recipients = getAdapter().getRecipients();
-    for(Recipient recipient : recipients) {
-      Address address = recipient.getAddress();
-      if(address.isDcContact()) {
-        int contactId = address.getDcContactId();
-        if(currentChatContactIds.indexOfKey(contactId) < 0) {
-          dcContext.addContactToChat(groupChatId, contactId);
-        } else {
-          currentChatContactIds.put(contactId, true);
-        }
-      }
-    }
-    for(int index = 0; index < currentChatContactIds.size(); index++) {
-      if (!currentChatContactIds.valueAt(index)) {
-        dcContext.removeContactFromChat(groupChatId, currentChatContactIds.keyAt(index));
-      }
-    }
-  }
-
-  private SelectedRecipientsAdapter getAdapter() {
-    return (SelectedRecipientsAdapter)lv.getAdapter();
+  private SelectedContactsAdapter getAdapter() {
+    return (SelectedContactsAdapter)lv.getAdapter();
   }
 
   private @Nullable String getGroupName() {
@@ -446,15 +330,13 @@ public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
         break;
 
       case PICK_CONTACT:
-        List<String> selected = data.getStringArrayListExtra("contacts");
-
-        for (String contact : selected) {
-          if(contact!=null) {
-            Address address = Address.fromSerialized(contact);
-            Recipient recipient = Recipient.from(this, address);
-            addSelectedContacts(recipient);
+        ArrayList<Integer> contactIds = new ArrayList<>();
+        for (String addr : Objects.requireNonNull(data.getStringArrayListExtra("contacts"))) {
+          if(addr != null) {
+            contactIds.add(dcContext.createContact(null, addr));
           }
         }
+        getAdapter().changeData(contactIds);
         break;
 
       case ScribbleActivity.SCRIBBLE_REQUEST_CODE:
@@ -481,64 +363,6 @@ public class GroupCreateActivity extends PassphraseRequiredActionBarActivity
                 setAvatar(output, resource);
               }
             });
-  }
-
-  private class AddRecipientButtonListener implements View.OnClickListener {
-    @Override
-    public void onClick(View v) {
-      Intent intent = new Intent(GroupCreateActivity.this, ContactMultiSelectionActivity.class);
-      intent.putExtra(ContactSelectionListFragment.SELECT_VERIFIED_EXTRA, verified);
-      ArrayList<String> preselectedContacts = new ArrayList<>();
-      Set<Recipient> recipients = GroupCreateActivity.this.getAdapter().getRecipients();
-      for (Recipient r : recipients) {
-        if(r.getAddress().isDcContact()) {
-          preselectedContacts.add(dcContext.getContact(r.getAddress().getDcContactId()).getAddr());
-        }
-      }
-      intent.putExtra(ContactSelectionListFragment.PRESELECTED_CONTACTS, preselectedContacts);
-      startActivityForResult(intent, PICK_CONTACT);
-    }
-  }
-
-  private static class FillExistingGroupInfoAsyncTask extends ProgressDialogAsyncTask<Integer,Void,Recipient> {
-
-    final GroupCreateActivity activity;
-
-    FillExistingGroupInfoAsyncTask(GroupCreateActivity activity) {
-      super(activity,
-            R.string.one_moment,
-            R.string.one_moment);
-      this.activity = activity;
-    }
-
-    @Override
-    protected Recipient doInBackground(Integer... recipientIds) {
-      Integer recipientsId = recipientIds[0];
-      return new Recipient(activity, activity.dcContext.getChat(recipientsId));
-    }
-
-    @Override
-    protected void onPostExecute(Recipient recipient) {
-      super.onPostExecute(recipient);
-      activity.fillExistingGroup(recipient);
-    }
-  }
-
-  private void fillExistingGroup(Recipient recipient) {
-    List<Recipient> participants = recipient.loadParticipants(this);
-    Recipient[] participantsArray = new Recipient[participants.size()];
-    participantsArray = participants.toArray(participantsArray);
-    if (!isFinishing()) {
-      addSelectedContacts(participantsArray);
-      groupName.setText(recipient.getName());
-      if (isEdit() && recipient.getName()!=null) {
-        groupName.setSelection(recipient.getName().length(), recipient.getName().length());
-      }
-      SelectedRecipientsAdapter adapter = new SelectedRecipientsAdapter(this, GlideApp.with(this), participants);
-      adapter.setOnRecipientDeletedListener(this);
-      lv.setAdapter(adapter);
-      updateViewState();
-    }
   }
 
   private <T> void setAvatar(T model, Bitmap bitmap) {
