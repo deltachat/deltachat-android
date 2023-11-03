071392387e09b099d8de52e7178dc9493865f93e
Asiel Díaz Benítez <asieldbenitez@gmail.com>
2023-11-03 17:34:09 -0400

    Update src/org/thoughtcrime/securesms/ProfileSettingsFragment.java

--- src/org/thoughtcrime/securesms/ProfileSettingsFragment.java
+++ src/org/thoughtcrime/securesms/ProfileSettingsFragment.java
@@ -239,7 +239,7 @@ public class ProfileSettingsFragment extends Fragment
   private void onVerifiedByClicked() {
     DcContact dcContact = dcContext.getContact(contactId);
     int verifierId = dcContact.getVerifierId();
-    if (verifierId != 0 && verifierId != DcContact.DC_CONTACT_ID_SELF && verifierId != contactId) {
+    if (verifierId != 0 && verifierId != DcContact.DC_CONTACT_ID_SELF) {
       Intent intent = new Intent(getContext(), ProfileActivity.class);
       intent.putExtra(ProfileActivity.CONTACT_ID_EXTRA, verifierId);
       startActivity(intent);
