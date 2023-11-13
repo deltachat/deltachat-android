6a76a3a7620b6870ea3cce1f538e655165540066
B. Petersen <r10s@b44t.com>
2023-11-13 23:20:27 +0100

    remove unused StorageUtil.canWriteInSignalStorageDir(), .canWriteInSignalStorageDir()

--- src/org/thoughtcrime/securesms/util/StorageUtil.java
+++ src/org/thoughtcrime/securesms/util/StorageUtil.java
@@ -17,28 +17,6 @@ import java.io.File;
 
 public class StorageUtil {
 
-  private static File getStorageDir() throws NoExternalStorageException {
-    final File storage = Environment.getExternalStorageDirectory();
-
-    if (!storage.canWrite()) {
-      throw new NoExternalStorageException();
-    }
-
-    return storage;
-  }
-
-  public static boolean canWriteInSignalStorageDir() {
-    File storage;
-
-    try {
-      storage = getStorageDir();
-    } catch (NoExternalStorageException e) {
-      return false;
-    }
-
-    return storage.canWrite();
-  }
-
   public static boolean canWriteToMediaStore(Context context) {
     return Build.VERSION.SDK_INT > 28 ||
             Permissions.hasAll(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
