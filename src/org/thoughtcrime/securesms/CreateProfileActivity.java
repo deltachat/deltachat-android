e3867311f06f90fc6d1a5723076fc9687c26aaa0
adbenitez <asieldbenitez@gmail.com>
2023-11-14 19:00:21 +0100

    remove unused view

--- src/org/thoughtcrime/securesms/CreateProfileActivity.java
+++ src/org/thoughtcrime/securesms/CreateProfileActivity.java
@@ -64,7 +64,6 @@ public class CreateProfileActivity extends BaseActionBarActivity implements Emoj
   private EditText               name;
   private MediaKeyboard          emojiDrawer;
   private EditText               statusView;
-  private View                   reveal;
 
   private boolean fromWelcome;
   private boolean avatarChanged;
@@ -206,7 +205,6 @@ public class CreateProfileActivity extends BaseActionBarActivity implements Emoj
     this.name         = ViewUtil.findById(this, R.id.name_text);
     this.emojiDrawer  = ViewUtil.findById(this, R.id.emoji_drawer);
     this.container    = ViewUtil.findById(this, R.id.container);
-    this.reveal       = ViewUtil.findById(this, R.id.reveal);
     this.statusView   = ViewUtil.findById(this, R.id.status_text);
 
     passwordAccountSettings.setOnClickListener(view -> {
