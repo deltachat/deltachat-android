353e1343e4638655ab330460cd587d2fdf5c4bbb
B. Petersen <r10s@b44t.com>
2024-02-18 16:46:23 +0100

    add 'show in chat' menu entry to 'saved messages'

--- src/main/java/org/thoughtcrime/securesms/ConversationFragment.java
+++ src/main/java/org/thoughtcrime/securesms/ConversationFragment.java
@@ -327,6 +327,7 @@ public class ConversationFragment extends MessageSelectorFragment
             menu.findItem(R.id.menu_context_reply).setVisible(false);
             menu.findItem(R.id.menu_context_reply_privately).setVisible(false);
             menu.findItem(R.id.menu_add_to_home_screen).setVisible(false);
+            menu.findItem(R.id.menu_show_in_chat).setVisible(false);
         } else {
             DcMsg messageRecord = messageRecords.iterator().next();
             DcChat chat = getListAdapter().getChat();
@@ -337,6 +338,7 @@ public class ConversationFragment extends MessageSelectorFragment
             boolean showReplyPrivately = chat.isMultiUser() && !messageRecord.isOutgoing() && canReply;
             menu.findItem(R.id.menu_context_reply_privately).setVisible(showReplyPrivately);
             menu.findItem(R.id.menu_add_to_home_screen).setVisible(messageRecord.getType() == DcMsg.DC_MSG_WEBXDC);
+            menu.findItem(R.id.menu_show_in_chat).setVisible(messageRecord.getOriginalMsg() != null);
         }
 
         // if one of the selected items cannot be saved, disable saving.
@@ -960,6 +962,9 @@ public class ConversationFragment extends MessageSelectorFragment
                     return true;
                 case R.id.menu_resend:
                     handleResendMessage(getListAdapter().getSelectedItems());
+                    return true;
+              case R.id.menu_show_in_chat:
+                    handleShowInChat(getSelectedMessageRecord(getListAdapter().getSelectedItems()).getOriginalMsg());
                     return true;
             }
 
