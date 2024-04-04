0f47e90dd013a7cd54e9a96bf3e25901d243aa4e
B. Petersen <r10s@b44t.com>
2024-04-04 11:25:23 +0200

    add support for reaction summaries

--- src/org/thoughtcrime/securesms/ConversationListFragment.java
+++ src/org/thoughtcrime/securesms/ConversationListFragment.java
@@ -119,6 +119,7 @@ public class ConversationListFragment extends Fragment
     eventCenter.addObserver(DcContext.DC_EVENT_MSG_DELIVERED, this);
     eventCenter.addObserver(DcContext.DC_EVENT_MSG_FAILED, this);
     eventCenter.addObserver(DcContext.DC_EVENT_MSG_READ, this);
+    eventCenter.addObserver(DcContext.DC_EVENT_REACTIONS_CHANGED, this);
     eventCenter.addObserver(DcContext.DC_EVENT_CONNECTIVITY_CHANGED, this);
     eventCenter.addObserver(DcContext.DC_EVENT_SELFAVATAR_CHANGED, this);
   }
