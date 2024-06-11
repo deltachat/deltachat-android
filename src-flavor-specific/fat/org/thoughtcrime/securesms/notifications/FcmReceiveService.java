57fb628445e849b0adadb920b9df2a0e2a7e35f6
B. Petersen <r10s@b44t.com>
2024-06-11 23:54:00 +0200

    for FOSS build, create an empty FcmReceiveService.java

---
+++ src-flavor-specific/fat/org/thoughtcrime/securesms/notifications/FcmReceiveService.java
@@ -0,0 +1,20 @@
+package org.thoughtcrime.securesms.notifications;
+
+import android.content.Context;
+import androidx.annotation.Nullable;
+
+public class FcmReceiveService {
+  public static void register(Context context) {
+  }
+
+  public static void waitForRegisterFinished() {
+  }
+
+  @Nullable
+  public static String getToken() {
+    return null;
+  }
+
+  public static void deleteToken() {
+  }
+}
