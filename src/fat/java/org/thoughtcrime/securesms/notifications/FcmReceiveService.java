fee58e6eacf712525267e5479aae45ba4a6ffbb1
adbenitez <asieldbenitez@gmail.com>
2024-06-12 02:17:10 +0200

    fix building

---
+++ src/fat/java/org/thoughtcrime/securesms/notifications/FcmReceiveService.java
@@ -0,0 +1,11 @@
+package org.thoughtcrime.securesms.notifications;
+
+import android.content.Context;
+import androidx.annotation.Nullable;
+
+public class FcmReceiveService {
+  public static void register(Context context) {}
+  public static void waitForRegisterFinished() {}
+  public static void deleteToken() {}
+  @Nullable public static String getToken() { return null; }
+}
