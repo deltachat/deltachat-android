bd669edf4e40ca193307386345e5878b50d18ec8
adbenitez <asieldbenitez@gmail.com>
2024-06-12 22:17:54 +0200

    update scripts to new res and assets folder location

--- scripts/tx-pull-source.sh
+++ scripts/tx-pull-source.sh
@@ -1,4 +1,5 @@
+RES=src/main/res
 tx pull -l en
-mv res/values-en/strings.xml res/values/strings.xml
-rmdir res/values-en
+mv $RES/values-en/strings.xml $RES/values/strings.xml
+rmdir $RES/values-en
 ./scripts/check-translations.sh
