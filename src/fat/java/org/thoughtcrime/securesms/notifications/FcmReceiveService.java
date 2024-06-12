6681f24603309bad8d6a0cc80a33c2981b5e059a
adbenitez <asieldbenitez@gmail.com>
2024-06-12 02:28:11 +0200

    add comment to fake implementation of FcmReceiveService

--- src/fat/java/org/thoughtcrime/securesms/notifications/FcmReceiveService.java
+++ src/fat/java/org/thoughtcrime/securesms/notifications/FcmReceiveService.java
@@ -3,6 +3,10 @@ package org.thoughtcrime.securesms.notifications;
 import android.content.Context;
 import androidx.annotation.Nullable;
 
+/*
+  Fake do-nothing implementation of FcmReceiveService.
+  The real implementation is in the gplay flavor only.
+*/
 public class FcmReceiveService {
   public static void register(Context context) {}
   public static void waitForRegisterFinished() {}
