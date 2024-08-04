0db771d00d3797eeb427b88e2fb8c1bc65ce5f2f
B. Petersen <r10s@b44t.com>
2024-08-04 19:27:02 +0200

    remove now dead code

--- src/main/java/org/thoughtcrime/securesms/mms/AttachmentManager.java
+++ src/main/java/org/thoughtcrime/securesms/mms/AttachmentManager.java
@@ -474,16 +474,6 @@ public class AttachmentManager {
             .execute();
   }
 
-  public static void selectAudio(Activity activity, int requestCode) {
-    Permissions.with(activity)
-               .request(Manifest.permission.READ_EXTERNAL_STORAGE)
-               .alwaysGrantOnSdk30()
-               .ifNecessary()
-               .withPermanentDenialDialog(activity.getString(R.string.perm_explain_access_to_storage_denied))
-               .onAllGranted(() -> selectMediaType(activity, "audio/*", null, requestCode))
-               .execute();
-  }
-
   public static void selectLocation(Activity activity, int chatId) {
     ApplicationContext applicationContext = ApplicationContext.getInstance(activity);
     DcLocationManager dcLocationManager = applicationContext.dcLocationManager;
