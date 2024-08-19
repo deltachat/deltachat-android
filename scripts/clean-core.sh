126552b4195cd7f7db57b602da551bea721124f4
B. Petersen <r10s@b44t.com>
2024-08-19 22:16:32 +0200

    clean content of obj and build folders
    explicitly list the content to be deleted,
    to have some more general protection in case sth. goes wrong accidentally ...

--- scripts/clean-core.sh
+++ scripts/clean-core.sh
@@ -2,6 +2,12 @@ cd jni/deltachat-core-rust
 cargo clean
 cd -
 
+rm -rf build/generated
+rm -rf build/intermediates
+rm -rf build/outputs
+rm -rf build/tmp
+rm     build/gmpAppId.txt
+rm -rf obj/local
 rm -rf jni/deltachat-core-rust/target
 rm jni/armeabi-v7a/libdeltachat.a
 rm jni/x86/libdeltachat.a
