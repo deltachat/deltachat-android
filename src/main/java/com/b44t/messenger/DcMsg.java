bf0ee26cc74e0bdb8ce8564be591f7cb4c1d614f
Hocuri <hocuri@gmx.de>
2024-11-23 20:50:34 +0100

    Always show the "~" before the sender name if it's overridden
    When we introduced this, I assume that we weren't sure whether we should
    do it and only showed it in some places. But I think it's nicer to
    show the same sender name everywhere, i.e. always add the "~".

--- src/main/java/com/b44t/messenger/DcMsg.java
+++ src/main/java/com/b44t/messenger/DcMsg.java
@@ -166,10 +166,10 @@ public class DcMsg {
     public native String  getError           ();
     public native String  getOverrideSenderName();
 
-    public String getSenderName(DcContact dcContact, boolean markOverride) {
+    public String getSenderName(DcContact dcContact) {
         String overrideName = getOverrideSenderName();
         if (overrideName != null) {
-            return (markOverride ? "~" : "") + overrideName;
+            return "~" + overrideName;
         } else {
             return dcContact.getDisplayName();
         }
