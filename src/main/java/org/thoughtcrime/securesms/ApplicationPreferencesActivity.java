9d96d7311c16a9c3f4d34ba364a1baf925ad75f4
adbenitez <asieldbenitez@gmail.com>
2025-02-16 21:47:25 +0100

    revert removal of comments

--- src/main/java/org/thoughtcrime/securesms/ApplicationPreferencesActivity.java
+++ src/main/java/org/thoughtcrime/securesms/ApplicationPreferencesActivity.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2011 Whisper Systems
+ * Copyright (C) 2013-2017 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.thoughtcrime.securesms;
 
 import android.content.Intent;
@@ -30,6 +47,13 @@ import org.thoughtcrime.securesms.util.DynamicTheme;
 import org.thoughtcrime.securesms.util.Prefs;
 import org.thoughtcrime.securesms.qr.BackupTransferActivity;
 import org.thoughtcrime.securesms.util.ScreenLockUtil;
+
+/**
+ * The Activity for application preference display and management.
+ *
+ * @author Moxie Marlinspike
+ *
+ */
 
 public class ApplicationPreferencesActivity extends PassphraseRequiredActionBarActivity
     implements SharedPreferences.OnSharedPreferenceChangeListener
