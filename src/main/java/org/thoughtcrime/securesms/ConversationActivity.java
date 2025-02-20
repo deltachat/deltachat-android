3995cc2830ff6448b5440ee9517ec0b14094da8b
adbenitez <asieldbenitez@gmail.com>
2025-02-20 18:42:18 +0100

    fix: don't discard draft if not in editing mode

--- src/main/java/org/thoughtcrime/securesms/ConversationActivity.java
+++ src/main/java/org/thoughtcrime/securesms/ConversationActivity.java
@@ -1267,8 +1267,8 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
 
   @Override
   public void onQuoteDismissed() {
+    if (isEditing) composeText.setText("");
     isEditing = false;
-    composeText.setText("");
   }
 
   // media selected by the system keyboard
