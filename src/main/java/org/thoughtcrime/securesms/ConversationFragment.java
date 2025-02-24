c5d7ec09463b5a663cbdd49b1a1f42629ae2d587
adbenitez <asieldbenitez@gmail.com>
2025-02-24 17:11:04 +0100

    don't allow to edit html messages

--- src/main/java/org/thoughtcrime/securesms/ConversationFragment.java
+++ src/main/java/org/thoughtcrime/securesms/ConversationFragment.java
@@ -365,7 +365,7 @@ public class ConversationFragment extends MessageSelectorFragment
     }
 
     static boolean canEditMsg(DcMsg dcMsg) {
-        return dcMsg.isOutgoing() && !dcMsg.isInfo() && dcMsg.getType() != DcMsg.DC_MSG_VIDEOCHAT_INVITATION && !dcMsg.getText().isEmpty();
+        return dcMsg.isOutgoing() && !dcMsg.isInfo() && dcMsg.getType() != DcMsg.DC_MSG_VIDEOCHAT_INVITATION && !dcMsg.hasHtml() && !dcMsg.getText().isEmpty();
     }
 
     public void handleClearChat() {
