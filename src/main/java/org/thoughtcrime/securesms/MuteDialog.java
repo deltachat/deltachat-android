0dfbc83a037e574d20e4d7303e073282dde38ffc
B. Petersen <r10s@b44t.com>
2025-03-13 16:50:16 +0100

    add mute option for 8 hours

--- src/main/java/org/thoughtcrime/securesms/MuteDialog.java
+++ src/main/java/org/thoughtcrime/securesms/MuteDialog.java
@@ -19,7 +19,7 @@ public class MuteDialog {
       // See https://c.delta.chat/classdc__context__t.html#a6460395925d49d2053bc95224bf5ce37.
       switch (which) {
         case 0:  muteUntil = TimeUnit.HOURS.toSeconds(1); break;
-        case 1:  muteUntil = TimeUnit.HOURS.toSeconds(2); break;
+        case 1:  muteUntil = TimeUnit.HOURS.toSeconds(8); break;
         case 2:  muteUntil = TimeUnit.DAYS.toSeconds(1);  break;
         case 3:  muteUntil = TimeUnit.DAYS.toSeconds(7);  break;
         case 4:  muteUntil = -1; break; // mute forever
