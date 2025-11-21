57ac9d6b916ff55298fb185d83cd57a2a81483a5
adbenitez <asieldbenitez@gmail.com>
2025-11-21 03:11:30 +0100

    don't linkify numbers in the middle of text

--- src/main/java/org/thoughtcrime/securesms/util/Linkifier.java
+++ src/main/java/org/thoughtcrime/securesms/util/Linkifier.java
@@ -4,7 +4,6 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.URLSpan;
 import android.text.util.Linkify;
-import android.util.Patterns;
 
 import java.util.regex.Pattern;
 
@@ -14,11 +13,24 @@ public class Linkifier {
   private static final Pattern PROXY_PATTERN = Pattern.compile("(?<=^|\\s)(SOCKS5|socks5|ss|SS):[^ \\n]+");
   private static final Pattern PHONE_PATTERN
     = Pattern.compile(                   // sdd = space, dot, or dash
-        "(?<![a-zA-Z0-9])"               // no letter at start
+        "(?<=^|\\s|\\.|\\()"             // no letter at start
         + "(\\+[0-9]+[\\- \\.]*)?"       // +<digits><sdd>*
         + "(\\([0-9]+\\)[\\- \\.]*)?"    // (<digits>)<sdd>*
         + "([0-9][0-9\\- \\.]{3,}[0-9])" // <digit><digit|sdd>+<digit> (5 characters min)
-        + "(?![a-zA-Z0-9])");            // no letter at end
+        + "(?=$|\\s|\\.|\\))");          // no letter at end
+
+  private static int internalPhoneLinkifier = -1;
+
+  private static boolean internalPhoneLinkifierNeeded() {
+    if (internalPhoneLinkifier == -1) {
+      if(Linkify.addLinks(new SpannableString("a100b"), Linkify.PHONE_NUMBERS)) {
+        internalPhoneLinkifier = 1;
+      } else {
+        internalPhoneLinkifier = 0;
+      }
+    }
+    return internalPhoneLinkifier == 1;
+  }
 
   private static void replaceURLSpan(SpannableString messageBody) {
     URLSpan[] urlSpans = messageBody.getSpans(0, messageBody.length(), URLSpan.class);
@@ -41,12 +53,18 @@ public class Linkifier {
       replaceURLSpan(messageBody); // replace URLSpan so that it is not removed on the next addLinks() call
     }
 
-    if (Linkify.addLinks(messageBody, PHONE_PATTERN, "tel:", Linkify.sPhoneNumberMatchFilter, Linkify.sPhoneNumberTransformFilter)) {
-      replaceURLSpan(messageBody); // replace URLSpan so that it is not removed on the next addLinks() call
+    int flags;
+    if (internalPhoneLinkifierNeeded()) {
+      if (Linkify.addLinks(messageBody, PHONE_PATTERN, "tel:", Linkify.sPhoneNumberMatchFilter, Linkify.sPhoneNumberTransformFilter)) {
+        replaceURLSpan(messageBody); // replace URLSpan so that it is not removed on the next addLinks() call
+      }
+      flags = Linkify.EMAIL_ADDRESSES|Linkify.WEB_URLS;
+    } else {
+      flags = Linkify.EMAIL_ADDRESSES|Linkify.WEB_URLS|Linkify.PHONE_NUMBERS;
     }
 
     // linkyfiy urls etc., this removes all existing URLSpan
-    if (Linkify.addLinks(messageBody, Linkify.EMAIL_ADDRESSES|Linkify.WEB_URLS)) {
+    if (Linkify.addLinks(messageBody, flags)) {
       replaceURLSpan(messageBody);
     }
 
