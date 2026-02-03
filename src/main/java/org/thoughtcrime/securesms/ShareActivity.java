15c60c6b12612029fe22e1dca70643b963691528
adbenitez <asieldbenitez@gmail.com>
2026-02-03 20:57:47 +0100

    fix mailto handling

--- src/main/java/org/thoughtcrime/securesms/ShareActivity.java
+++ src/main/java/org/thoughtcrime/securesms/ShareActivity.java
@@ -104,24 +104,23 @@ public class ShareActivity extends PassphraseRequiredActionBarActivity implement
     resolvedExtras = new ArrayList<>();
 
     List<Uri> streamExtras = new ArrayList<>();
+    if (MailtoUtil.isMailto(getIntent().getData())) {
+      String[] extraEmail = getIntent().getStringArrayExtra(Intent.EXTRA_EMAIL);
+      if (extraEmail == null || extraEmail.length == 0) {
+        getIntent().putExtra(Intent.EXTRA_EMAIL, MailtoUtil.getRecipients(getIntent().getData()));
+      }
+      String text = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+      if (text == null || text.isEmpty()) {
+        getIntent().putExtra(Intent.EXTRA_TEXT, MailtoUtil.getText(getIntent().getData()));
+      }
+    }
+
     if (Intent.ACTION_SEND.equals(getIntent().getAction()) &&
             getIntent().getParcelableExtra(Intent.EXTRA_STREAM) != null) {
         Uri uri = getIntent().getParcelableExtra(Intent.EXTRA_STREAM);
         streamExtras.add(uri);
     } else if (getIntent().getParcelableArrayListExtra(Intent.EXTRA_STREAM) != null) {
       streamExtras = getIntent().getParcelableArrayListExtra(Intent.EXTRA_STREAM);
-    } else {
-        Uri uri = getIntent().getData();
-        if (MailtoUtil.isMailto(uri)) {
-            String[] extraEmail = getIntent().getStringArrayExtra(Intent.EXTRA_EMAIL);
-            if (extraEmail == null || extraEmail.length == 0) {
-                getIntent().putExtra(Intent.EXTRA_EMAIL, MailtoUtil.getRecipients(uri));
-            }
-            String text = getIntent().getStringExtra(Intent.EXTRA_TEXT);
-            if (text == null || text.isEmpty()) {
-                getIntent().putExtra(Intent.EXTRA_TEXT, MailtoUtil.getText(uri));
-            }
-        }
     }
 
     if (needsFilePermission(streamExtras)) {
@@ -263,6 +262,7 @@ public class ShareActivity extends PassphraseRequiredActionBarActivity implement
       }
 
       chatId = dcContext.createChatByContactId(contactId);
+      accId = dcContext.getAccountId();
     }
     Intent composeIntent;
     if (accId != -1 && chatId != -1) {
