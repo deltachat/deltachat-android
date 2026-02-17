b365284743ecac7421b876afbc96559f9ff29d16
adbenitez <asieldbenitez@gmail.com>
2026-02-17 22:37:56 +0100

    fix Rpc class

--- src/main/java/chat/delta/rpc/Rpc.java
+++ src/main/java/chat/delta/rpc/Rpc.java
@@ -702,13 +702,8 @@ public class Rpc {
    * <p>
    * See also [`Self::get_chat_description`] / `getChatDescription()`.
    */
-<<<<<<< adb/chat-description
-  public void setChatDescription(Integer accountId, Integer chatId, String newDescription) throws RpcException {
-    transport.call("set_chat_description", mapper.valueToTree(accountId), mapper.valueToTree(chatId), mapper.valueToTree(newDescription));
-=======
   public void setChatDescription(Integer accountId, Integer chatId, String description) throws RpcException {
     transport.call("set_chat_description", mapper.valueToTree(accountId), mapper.valueToTree(chatId), mapper.valueToTree(description));
->>>>>>> main
   }
 
   /**
