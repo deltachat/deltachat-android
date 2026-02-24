2107eb50013c2a957202fb3aaa5c09baa7cf4cf2
sim <git@sgougeon.fr>
2026-02-24 16:54:43 +0100

    Add dialog to introduce UnifiedPush distrib picker if needed

--- src/main/java/org/thoughtcrime/securesms/ConversationListActivity.java
+++ src/main/java/org/thoughtcrime/securesms/ConversationListActivity.java
@@ -321,19 +321,60 @@ public class ConversationListActivity extends PassphraseRequiredActionBarActivit
       UnifiedPush.saveDistributor(this, ((ResolvedDistributor.Found) resolvedDistributor).getPackageName());
       ApplicationContext.getInstance(this).initializePush();
     } else if (resolvedDistributor instanceof ResolvedDistributor.ToSelect) {
-      //TODO(UP): Dialog
-      UnifiedPush.tryUseDefaultDistributor(this, success -> {
-        if (success) {
-          Prefs.resetReliableService(this);
-          ApplicationContext.getInstance(this).initializePush();
-        } else {
-          // The user has closed the OS dialog, we consider they don't want UnifiedPush
-          Prefs.setUnifiedPush(this, false);
-        }
-        return null;
-      });
+      selectUnifiedPushDistributor();
     }
     // Else do nothing: the periodic sync is already setup during ApplicationContext init
+  }
+
+  /**
+   * The user has many distributors installed on the system, and none of them is defined as the
+   * default one: it needs to be selected with the OS dialog
+   *
+   * <p>To avoid the OS dialog to pop from nowhere, we introduce it with a simple dialog to show
+   * "You're about to select your UnifiedPush service." The OS dialog has a background showing
+   * "UnifiedPush" but some users may not see it. It is better to introduce with plain text.</p>
+   *
+   * <p>Note: The user necessarily knows about UnifiedPush: they have installed and enabled UnifiedPush
+   * on at least 2 distributors.</p>
+   */
+  private void selectUnifiedPushDistributor() {
+    DialogCallback callback = new DialogCallback() {
+      private Activity context = ConversationListActivity.this;
+      @Override
+      public void onCancel() {
+        Prefs.setUnifiedPush(context, false);
+      }
+
+      @Override
+      public void onConfirm() {
+        UnifiedPush.tryUseDefaultDistributor(context, success -> {
+          if (success) {
+            Prefs.resetReliableService(context);
+            ApplicationContext.getInstance(context).initializePush();
+          } else {
+            // The user has closed the OS dialog, we consider they don't want UnifiedPush
+            Prefs.setUnifiedPush(context, false);
+          }
+          return null;
+        });
+      }
+    };
+
+    introduceUnifiedPushDialog(callback);
+  }
+
+  private interface DialogCallback{
+    void onCancel();
+    void onConfirm();
+  }
+
+  private void introduceUnifiedPushDialog(DialogCallback callback) {
+    new AlertDialog.Builder(this)
+      .setMessage(R.string.dialog_introduce_unifiedpush_selection)
+      .setCancelable(true)
+      .setNegativeButton(android.R.string.cancel, (_d, _i) -> callback.onCancel())
+      .setPositiveButton(android.R.string.ok, (_d, _i) -> callback.onConfirm())
+      .show();
   }
 
   @Override
