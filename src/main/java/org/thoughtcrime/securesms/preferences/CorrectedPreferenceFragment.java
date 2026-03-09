b9461259d7658c23527978ce5fbfde704c5b534d
B. Petersen <r10s@b44t.com>
2026-03-09 12:49:00 +0100

    remove now superfluous null check

--- src/main/java/org/thoughtcrime/securesms/preferences/CorrectedPreferenceFragment.java
+++ src/main/java/org/thoughtcrime/securesms/preferences/CorrectedPreferenceFragment.java
@@ -1,16 +1,11 @@
 package org.thoughtcrime.securesms.preferences;
 
-
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.DialogFragment;
-import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public abstract class CorrectedPreferenceFragment extends PreferenceFragmentCompat {
-
   @Override
   public void onCreate(Bundle icicle) {
     super.onCreate(icicle);
@@ -23,18 +18,4 @@ public abstract class CorrectedPreferenceFragment extends PreferenceFragmentComp
     View lv = getView().findViewById(android.R.id.list);
     if (lv != null) lv.setPadding(0, 0, 0, 0);
   }
-
-  @Override
-  public void onDisplayPreferenceDialog(@NonNull Preference preference) {
-    DialogFragment dialogFragment = null;
-
-    if (dialogFragment != null) {
-      dialogFragment.setTargetFragment(this, 0);
-      dialogFragment.show(getFragmentManager(), "android.support.v7.preference.PreferenceFragment.DIALOG");
-    } else {
-      super.onDisplayPreferenceDialog(preference);
-    }
-  }
-
-
 }
