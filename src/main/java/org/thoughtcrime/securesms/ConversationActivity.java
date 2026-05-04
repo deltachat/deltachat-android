2ab544b18e8c08e2f496bf6fa92c4bcd5de9a1b1
adbenitez <asieldbenitez@gmail.com>
2026-05-04 19:55:13 +0200

    don't set draft if user can't send

--- src/main/java/org/thoughtcrime/securesms/ConversationActivity.java
+++ src/main/java/org/thoughtcrime/securesms/ConversationActivity.java
@@ -674,8 +674,14 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   }
 
   public void setDraftText(String txt) {
-    composeText.setText(txt);
-    composeText.setSelection(composeText.getText().length());
+    try {
+      if (rpc.canSend(rpc.getSelectedAccountId(), chatId)) {
+        composeText.setText(txt);
+        composeText.setSelection(composeText.getText().length());
+      }
+    } catch (RpcException e) {
+      Log.e(TAG, "Rpc error", e);
+    }
   }
 
   public void hideSoftKeyboard() {
@@ -1260,6 +1266,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
 
     DcContext dcContext = DcHelper.getContext(context);
     final int currentChatId = dcChat.getId();
+    final boolean canSend = dcChat.canSend();
     Util.runOnAnyBackgroundThread(
         () -> {
           DcMsg msg = null;
@@ -1378,7 +1385,8 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
               }
             }
           } else {
-            dcContext.setDraft(currentChatId, msg);
+            // set or clear draft if user can't send in chat since they can't delete it otherwise
+            dcContext.setDraft(currentChatId, canSend ? msg : null);
           }
           future.set(currentChatId);
         });
