a9049a037d0b94cde9772683fc2a369759488d21
wch423 <chwl1001@tuta.io>
2026-05-29 16:09:57 +0200

    Reset scroll position after switching account

--- src/main/java/org/thoughtcrime/securesms/ConversationListActivity.java
+++ src/main/java/org/thoughtcrime/securesms/ConversationListActivity.java
@@ -717,6 +717,7 @@ public class ConversationListActivity extends PassphraseRequiredActionBarActivit
     refreshAvatar();
     refreshUnreadIndicator();
     refreshTitle();
+    conversationListFragment.resetScrollPosition();
     conversationListFragment.loadChatlistAsync();
   }
 
