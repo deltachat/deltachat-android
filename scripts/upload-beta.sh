f16b630e2b8d308ec35bf95f487886e2372762d9
j4n <j4n@systemli.org>
2026-07-21 14:42:07 +0200

    chore(scripts/upload): More fixes for rsync
    - Use relative path
    - Exit early if upload fails

--- scripts/upload-beta.sh
+++ scripts/upload-beta.sh
@@ -21,9 +21,15 @@ ls -l $APKGPLAY
 ls -l $APKDEV
 read -p "upload these apks to download.delta.chat/android/beta? ENTER to continue, CTRL-C to abort."
 
-# If you want to be able to upload, send your public SSH key to sysadmin@testrun.org 
-rsync --progress $APKGPLAY www-android@download.delta.chat:/var/www/html/download/android/beta/
-rsync --progress $APKDEV www-android@download.delta.chat:/var/www/html/download/android/beta/
+# If you want to be able to upload, send your public SSH key to sysadmin@testrun.org
+if ! rsync --progress -e "ssh " "$APKGPLAY" www-android@download.delta.chat:/beta/; then
+    echo "upload of $APKGPLAY failed, aborting." >&2
+    exit 1
+fi
+if ! rsync --progress -e "ssh " "$APKDEV" www-android@download.delta.chat:/beta/; then
+    echo "upload of $APKDEV failed, aborting." >&2
+    exit 1
+fi
 
 echo "upload done."
 echo ""
