03559a1a13217c8b9880c4e354c5485372035c24
adbenitez <asieldbenitez@gmail.com>
2026-09-18 16:48:52 +0200

    don't use "addr" config

--- src/main/java/org/thoughtcrime/securesms/ConversationListActivity.java
+++ src/main/java/org/thoughtcrime/securesms/ConversationListActivity.java
@@ -378,8 +378,9 @@ public class ConversationListActivity extends PassphraseRequiredActionBarActivit
       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     } else {
       boolean multiProfile = DcHelper.getAccounts(this).getAll().length > 1;
-      String defText =
-          multiProfile ? DcHelper.getContext(this).getName() : getString(R.string.app_name);
+      String name = DcHelper.get(this, DcHelper.CONFIG_DISPLAY_NAME);
+      if (name.isEmpty()) name = getString(R.string.unnamed);
+      String defText = multiProfile ? name : getString(R.string.app_name);
       title.setText(DcHelper.getConnectivitySummary(this, defText));
       // refreshTitle is called by ConversationListFragment when connectivity changes so update
       // connectivity dot here
