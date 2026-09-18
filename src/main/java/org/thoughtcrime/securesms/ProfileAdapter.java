96b3dc2c7c7cfb5f372c5261f4a2fba87beee0ff
adbenitez <asieldbenitez@gmail.com>
2026-09-18 21:38:49 +0200

    fix variable name in ProfileAdapter

--- src/main/java/org/thoughtcrime/securesms/ProfileAdapter.java
+++ src/main/java/org/thoughtcrime/securesms/ProfileAdapter.java
@@ -183,7 +183,7 @@ public class ProfileAdapter extends RecyclerView.Adapter {
       } else {
         dcContact = dcContext.getContact(contactId);
         name = dcContact.getDisplayName();
-        if (!contact.isKeyContact()) subtitle = dcContact.getAddr();
+        if (!dcContact.isKeyContact()) subtitle = dcContact.getAddr();
       }
 
       contactItem.unbind(glideRequests);
